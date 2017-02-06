0543ffdf518a4f51da2ef35b5d4224de2e85fd0f
geiner salcedo <geinersalcedo@localhost.localdomain>
2017-02-06 12:39:53 -0500

    Cliente financiera

---
+++ app/scripts/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name financieraClienteApp
+ * @description
+ * # financieraClienteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('financieraClienteApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngTouch',
+    'afOAuth2',
+    'treeControl',
+    'ngMaterial',
+    'ui.grid',
+    'ui.grid.edit',
+    'ui.grid.rowEdit',
+    'ui.grid.cellNav',
+    'ui.grid.treeView',
+    'ui.grid.selection',
+    'ui.grid.exporter',
+    'ngStorage'
+  ])
+    .config(['$locationProvider','$routeProvider', function($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix("");
+      $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
