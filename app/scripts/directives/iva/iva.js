321160f42b2ccfab2f615326207dfabca7fb3907
jotavargas <jotavargas89@gmial.com>
2017-02-25 23:53:39 -0500

    9779 ajustes a directiva para funcionalidad de crear

--- app/scripts/directives/iva/iva.js
+++ app/scripts/directives/iva/iva.js
@@ -16,7 +16,11 @@ angular.module('financieraClienteApp')
       */
       templateUrl: 'views/directives/iva/iva.html',
       controller:function(){
+        //get categorias
         var self = this;
+        self.poder_crear_iva = false;
+        self.categoria_selet = {};
+        self.nuevo_iva = null;
 
         financieraRequest.get("categoria_iva",
           $.param({
@@ -25,19 +29,36 @@ angular.module('financieraClienteApp')
         ).then(function(response) {
           self.categorias = response.data;
         });
-
         //get ivas de una categoria
-        self.cargar_ivas = function(categoria_id){
-          console.log(categoria_id)
+        self.cargar_ivas = function(categoria){
+          //console.log(categoria);
+          self.categoria_selet = categoria;
+          self.iva = {};
           financieraRequest.get("iva",
             $.param({
-              query:"estado_activo:True,categoria_iva:"+categoria_id,
+              query:"estado_activo:True,categoria_iva:"+categoria.Id,
             })
           ).then(function(response) {
             self.ivas = response.data;
           });
         };
+        // get iva
+        self.editar_iva=function(iva){
+          self.ver_add_iva = !self.ver_add_iva ;
+          self.iva = iva;
+        }
+        // add iva
+        self.agregar_iva=function(){
+          if(self.nuevo_iva){
+            self.nuevo_iva.CategoriaIva = self.categoria_selet;
+            self.nuevo_iva.EstadoActivo = true;
+            financieraRequest.post("iva", self.nuevo_iva).then(function(response){
+              self.cargar_ivas(self.categoria_selet)
+              self.nuevo_iva=null;
+            });
 
+          }
+        }
       },
       controllerAs:'d_iva'
     };
