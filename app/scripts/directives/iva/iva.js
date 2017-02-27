180c7e9313e51c77dfd449850f4d83acbb5e968d
jotavargas <jotavargas89@gmial.com>
2017-02-27 10:52:59 -0500

    #9779 grid para directiva iva

--- app/scripts/directives/iva/iva.js
+++ app/scripts/directives/iva/iva.js
@@ -14,20 +14,45 @@ angular.module('financieraClienteApp')
           var:'='
         },
       */
+
       templateUrl: 'views/directives/iva/iva.html',
-      controller:function(){
-        //get categorias
+      controller:function($scope){
         var self = this;
         self.poder_crear_iva = false;
         self.categoria_selet = {};
         self.nuevo_iva = null;
-
+        // grid
+        $scope.gridIvaCategoria = {
+          enableFiltering: true,
+          onRegisterApi: function(gridApi){
+            $scope.gridApi = gridApi;
+          }
+        };
+        $scope.gridIvaCategoria.columnDefs = [
+          {
+            name: 'Id',
+            displayName: "Id",
+          },
+          {
+            name: 'Nombre',
+            displayName: "Nombre",
+          },
+          {
+            //<button class="btn primary" ng-click="grid.appScope.deleteRow(row)">Delete</button>
+            name: 'Operacion',
+             enableFiltering: false,
+            cellTemplate:
+            '<button type="button" class="btn btn-danger btn-circle"><i class="fa fa-times"></i></button>'
+          }
+        ];
+        //get categorias
         financieraRequest.get("categoria_iva",
           $.param({
             query:"estado_activo:True"
           })
         ).then(function(response) {
           self.categorias = response.data;
+          $scope.gridIvaCategoria.data = response.data;
         });
         //get ivas de una categoria
         self.cargar_ivas = function(categoria){
