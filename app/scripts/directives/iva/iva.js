cbc4493c177540f9a6c1165845947292ccdc4f45
jotavargas <jotavargas89@gmial.com>
2017-02-28 00:22:12 -0500

    #9779 cambios en la definicion de las variables de la directiva para poder trabajar con ui-grid

--- app/scripts/directives/iva/iva.js
+++ app/scripts/directives/iva/iva.js
@@ -18,10 +18,12 @@ angular.module('financieraClienteApp')
       templateUrl: 'views/directives/iva/iva.html',
       controller:function($scope){
         var self = this;
-        self.poder_crear_iva = false;
-        self.categoria_selet = {};
-        self.nuevo_iva = null;
-        // grid
+        $scope.boton_crear_iva = false;
+        $scope.categoria_selet = {};
+        $scope.nuevo_iva = null;
+        $scope.actualizar_iva=null;
+
+        // grid Iva Categoria
         $scope.gridIvaCategoria = {
           enableFiltering: true,
           onRegisterApi: function(gridApi){
@@ -42,7 +44,35 @@ angular.module('financieraClienteApp')
             name: 'Operacion',
              enableFiltering: false,
             cellTemplate:
-            '<button type="button" class="btn btn-danger btn-circle"><i class="fa fa-times"></i></button>'
+            '<center><button ng-click="grid.appScope.cargar_ivas(row)" type="button" class="btn btn-success btn-circle"><i class="glyphicon glyphicon-eye-open""></i></button></center>'
+          }
+        ];
+        // grid Iva Categoria
+        $scope.gridIva = {
+          enableFiltering: true,
+          onRegisterApi: function(gridApi){
+            $scope.gridApi = gridApi;
+          }
+        };
+        $scope.gridIva.columnDefs = [
+          {
+            name: 'Id',
+            displayName: "Id",
+          },
+          {
+            name: 'CategoriaIva.Nombre',
+            displayName: "Categoria",
+          },
+          {
+            name: 'Valor',
+            displayName: "Valor",
+          },
+          {
+            //<button class="btn primary" ng-click="grid.appScope.deleteRow(row)">Delete</button>
+            name: 'Operacion',
+             enableFiltering: false,
+            cellTemplate:
+            '<center><button ng-click="grid.appScope.get_iva(row)" type="button" class="btn btn-success btn-circle"><i class="glyphicon glyphicon-pencil""></i></button></center>'
           }
         ];
         //get categorias
@@ -51,37 +81,52 @@ angular.module('financieraClienteApp')
             query:"estado_activo:True"
           })
         ).then(function(response) {
-          self.categorias = response.data;
           $scope.gridIvaCategoria.data = response.data;
         });
         //get ivas de una categoria
-        self.cargar_ivas = function(categoria){
+        $scope.cargar_ivas = function(categoria){
           //console.log(categoria);
-          self.categoria_selet = categoria;
-          self.iva = {};
+          $scope.categoria_selet = categoria.entity;
+          $scope.boton_crear_iva=true;
           financieraRequest.get("iva",
             $.param({
-              query:"estado_activo:True,categoria_iva:"+categoria.Id,
+              query:"estado_activo:True,categoria_iva:"+categoria.entity.Id,
+              limit:0,
             })
           ).then(function(response) {
-            self.ivas = response.data;
+            $scope.gridIva.data = response.data;
           });
         };
         // get iva
-        self.editar_iva=function(iva){
-          self.ver_add_iva = !self.ver_add_iva ;
-          self.iva = iva;
+        $scope.get_iva=function(iva){
+          //console.log(iva);
+          if ($scope.ver_crear_iva == true){
+            $scope.ver_crear_iva = !$scope.ver_crear_iva;
+          }
+          $scope.boton_crear_iva = !$scope.boton_crear_iva;
+          $scope.var_actualizar_iva = !$scope.var_actualizar_iva;
+          //
+          $scope.actualizar_iva = iva.entity;
+        }
+        // update iva
+        $scope.actualiza_iva=function(){
+          if($scope.actualizar_iva){
+            financieraRequest.put("iva",$scope.actualizar_iva.Id ,$scope.actualizar_iva).then(function(response){
+              console.log(response.data)
+              $scope.cargar_ivas({'name': 'hola', 'entity': $scope.categoria_selet})
+              $scope.actualizar_iva=null;
+            });
+          }
         }
         // add iva
-        self.agregar_iva=function(){
-          if(self.nuevo_iva){
-            self.nuevo_iva.CategoriaIva = self.categoria_selet;
-            self.nuevo_iva.EstadoActivo = true;
-            financieraRequest.post("iva", self.nuevo_iva).then(function(response){
-              self.cargar_ivas(self.categoria_selet)
-              self.nuevo_iva=null;
+        $scope.agregar_iva=function(){
+          if($scope.nuevo_iva){
+            $scope.nuevo_iva.CategoriaIva = $scope.categoria_selet;
+            $scope.nuevo_iva.EstadoActivo = true;
+            financieraRequest.post("iva", $scope.nuevo_iva).then(function(response){
+              $scope.cargar_ivas({'name': 'hola', 'entity': $scope.categoria_selet})
+              $scope.nuevo_iva=null;
             });
-
           }
         }
       },
