4a98f303e93e60dbd5cb6e2ed41784370e6cb76e
jotavargas <jotavargas89@gmial.com>
2017-03-04 21:09:44 -0500

    #10058 desarrollo directiva de rp por entrada del id proveedor

--- app/scripts/directives/rp/rp_por_proveedor_listar.js
+++ app/scripts/directives/rp/rp_por_proveedor_listar.js
@@ -7,17 +7,50 @@
  * # rp/rpPorProveedorListar
  */
 angular.module('financieraClienteApp')
-  .directive('rpPorProveedorListar', function () {
+  .directive('rpPorProveedorListar', function (financieraRequest) {
     return {
       restrict: 'E',
-      /*scope:{
-          var:'='
+      scope:{
+          beneficiaroid:'='
         },
-      */
+
       templateUrl: '/views/directives/rp/rp_por_proveedor_listar.html',
-      controller:function(){
-        var ctrl = this;
+      controller:function($scope){
+        var self = this;
+        self.gridOptions_proveedor = {
+          enableRowSelection: true,
+          enableRowHeaderSelection: false,
+          enableFiltering: true,
+
+          columnDefs : [
+            {field: 'Id',                             visible : false},
+            {field: 'NumeroRegistroPresupuestal',     displayName: 'Numero RP'},
+            {field: 'Estado.Nombre',                  displayName: 'Estado'},
+            {field: 'Vigencia',                       displayName: 'Vigencia'}
+          ]
+        };
+        //$scope.$watch('escritura', function(){
+        //})
+
+        financieraRequest.get('registro_presupuestal',
+          $.param({
+              query: "Beneficiario:3",
+              limit: 0,
+          })).then(function(response) {
+            self.gridOptions_proveedor.data = response.data;
+        });
+
+        self.gridOptions_proveedor.onRegisterApi = function(gridApi){
+            //set gridApi on scope
+            self.gridApi = gridApi;
+            gridApi.selection.on.rowSelectionChanged($scope,function(row){
+              $scope.proveedor = row.entity
+            });
+          };
+          self.gridOptions_proveedor.multiSelect = false;
+
+      //
       },
-      controllerAs:'d_rp/rpPorProveedorListar'
+      controllerAs:'d_rpPorProveedorListar'
     };
   });
