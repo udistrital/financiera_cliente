e1afd2ecbeee5eea316b96d38200dfaab0937df2
jotavargas <jotavargas89@gmial.com>
2017-03-04 23:00:07 -0500

    #10058 detalle para rp seleccionado

--- app/scripts/directives/rp/rp_por_proveedor_listar.js
+++ app/scripts/directives/rp/rp_por_proveedor_listar.js
@@ -18,7 +18,7 @@ angular.module('financieraClienteApp')
       templateUrl: '/views/directives/rp/rp_por_proveedor_listar.html',
       controller:function($scope){
         var self = this;
-        self.gridOptions_proveedor = {
+        self.gridOptions_rp = {
           enableRowSelection: true,
           enableRowHeaderSelection: false,
           enableFiltering: true,
@@ -46,22 +46,28 @@ angular.module('financieraClienteApp')
                   query: "Beneficiario:" + $scope.beneficiaroid,
                   limit: 0,
               })).then(function(response) {
-                self.gridOptions_proveedor.data = response.data;
+                self.gridOptions_rp.data = response.data;
             });
           }
         })
 
-
-        self.gridOptions_proveedor.onRegisterApi = function(gridApi){
+        self.gridOptions_rp.onRegisterApi = function(gridApi){
             //set gridApi on scope
             self.gridApi = gridApi;
             gridApi.selection.on.rowSelectionChanged($scope,function(row){
               $scope.rpselect = row.entity;
-              //consulta rp
-
+              //consulta datos del rp
+              financieraRequest.get('registro_presupuestal_disponibilidad_apropiacion',  //en el futuro será una servició con calculo de suma total
+                $.param({
+                    query: "RegistroPresupuestal.Id:" + $scope.rpselect.Id,
+                    limit: 0,
+                })).then(function(response) {
+                  self.rp_select_de_consulta = response.data;
+              });
+              //Fin consulta datos del rp
             });
           };
-          self.gridOptions_proveedor.multiSelect = false;
+          self.gridOptions_rp.multiSelect = false;
 
       //
       },
