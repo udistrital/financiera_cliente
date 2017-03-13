cc6ba5ea7a94d20d6d8a905e248e4b66a2aa02ad
jotavargas <jotavargas89@gmail.com>
2017-03-13 10:36:37 -0500

    debug insert orden pago

--- app/scripts/controllers/orden_pago/op_crear.js
+++ app/scripts/controllers/orden_pago/op_crear.js
@@ -8,7 +8,7 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('OrdenPagoOpCrearCtrl', function ($scope) {
+  .controller('OrdenPagoOpCrearCtrl', function ($scope, financieraRequest) {
     //
     var self = this;
     //unidad ejecutora
@@ -17,6 +17,24 @@ angular.module('financieraClienteApp')
     self.RubrosIds = [];
     self.RubrosIdsTest = [35354, 41];
     self.Concepto = [];
+
+    // Insert Orden Pago
+    self.addOpProveedor = function(){
+      self.OrdenPago.EstadoOrdenPago = {'Id': 1};
+      self.OrdenPago.Vigencia = 2017;
+      self.OrdenPago.PersonaElaboro = 1;
+
+      console.log("insert")
+      financieraRequest.post("orden_pago", self.OrdenPago)
+        .then(function(data) {   //error con el success
+          console.log(data)
+          self.resultado = data
+        })
+        /*.error(function(data, status, headers, config) {
+          console.log("error");
+        })*/
+    }
+
 
     //
   });
