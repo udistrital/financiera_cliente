4dfe11dfcdf88c4314a791407058169390054c2f
jotavargas <jotavargas89@gmail.com>
2017-03-13 13:56:15 -0500

    ajustes en transaccion

--- app/scripts/controllers/orden_pago/op_crear.js
+++ app/scripts/controllers/orden_pago/op_crear.js
@@ -18,12 +18,13 @@ angular.module('financieraClienteApp')
     self.RubrosIdsTest = [35354, 41];
     self.Concepto = [];
     self.ConceptoOrdenPago = [];
+    self.Data_OrdenPago_Concepto = {};
 
     // functions
     self.estructura_orden_pago_conceptos = function(conceptos){
       angular.forEach(conceptos, function(concepto){
         self.ConceptoOrdenPago.push({
-            'OrdenDePago': 0,
+            'OrdenDePago':{'Id': 0},
             'Concepto': {'Id': concepto.Id},
             'Valor': concepto.Afectacion
         });
@@ -36,6 +37,9 @@ angular.module('financieraClienteApp')
       self.OrdenPago.Vigencia = 2017;
       self.OrdenPago.PersonaElaboro = 1;
       // trabajar estructura de conceptos
+      self.Data_OrdenPago_Concepto = {};
+      self.ConceptoOrdenPago = [];
+      //
       if(self.Concepto ){
         self.estructura_orden_pago_conceptos(self.Concepto);
       }
@@ -44,13 +48,17 @@ angular.module('financieraClienteApp')
 
       // fin trabajar estructura de conceptos
       console.log("insert")
-      /*
-      financieraRequest.post("orden_pago", self.OrdenPago)
+      //construir data send
+      self.Data_OrdenPago_Concepto.OrdenPago = self.OrdenPago;
+      self.Data_OrdenPago_Concepto.ConceptoOrdenPago = self.ConceptoOrdenPago;
+      //insert
+
+      financieraRequest.post("orden_pago/RegistrarOp", self.Data_OrdenPago_Concepto)
         .then(function(data) {   //error con el success
           console.log(data)
           self.resultado = data
         })
-      */
+
         /*.error(function(data, status, headers, config) {
           console.log("error");
         })*/
