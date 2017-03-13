bd7bbae1156e3c9a25ba25345c9f5346b7b19c5f
Fabián Sánchez León <fadarsaleeing@gmail.com>
2017-03-13 09:50:08 -0500

    constantes de notificacion

---
+++ app/scripts/services/datos.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name financieraClienteApp.datos
+ * @description
+ * # datos
+ * Constant in the financieraClienteApp.
+ */
+angular.module('financieraClienteApp')
+  .constant('datos', {
+      "NOTIFICACION_PENDIENTE": "Notificaciones Pendientes"
+  });
