cc43c4f890266ccdd3b3b793f0ac9bdd75629800
jotavargas <jotavargas89@gmial.com>
2017-03-30 00:19:03 -0500

    ajustes_desarrollo #10572 de desarrolla detalle de conceptos y cuentas

--- app/scripts/directives/orden_pago/op_proveedor_ver_por_id.js
+++ app/scripts/directives/orden_pago/op_proveedor_ver_por_id.js
@@ -17,6 +17,7 @@ angular.module('financieraClienteApp')
       templateUrl: 'views/directives/orden_pago/op_proveedor_ver_por_id.html',
       controller:function($scope){
         var self = this;
+        self.rubros = [];
         //orden de pago
         $scope.$watch('opproveedorid', function(){
           if($scope.opproveedorid != undefined){
@@ -29,13 +30,14 @@ angular.module('financieraClienteApp')
                 self.asignar_proveedor(self.orden_pago[0].RegistroPresupuestal.Beneficiario)
                 //Iva
                 self.calcularIva(self.orden_pago[0].ValorBase, self.orden_pago[0].Iva.Valor)
+                //detalle concepto
+                self.detalle_concepto(self.orden_pago[0].Id)
+
             });
           }
         })
-
         // Function buscamos datos del proveedor que esta en el rp
         self.asignar_proveedor = function(beneficiario_id){
-          console.log(beneficiario_id);
           administrativaRequest.get('informacion_proveedor',
             $.param({ query: "Id:" + beneficiario_id,})
           ).then(function(response) {
@@ -46,6 +48,28 @@ angular.module('financieraClienteApp')
         self.calcularIva = function(valor_base, iva){
           self.ValorIva = ( parseInt(valor_base) * (parseInt(iva)/100) );
           self.ValorBruto = parseInt(valor_base) + parseInt(self.ValorIva);
+        }
+        // Function detall concepto
+        self.detalle_concepto = function(orden_pago_id){
+          financieraRequest.get('concepto_orden_pago',
+            $.param({ query: "OrdenDePago:" + orden_pago_id,})
+          ).then(function(response) {
+              self.conceptos = response.data;
+              self.detalle_rubros(self.conceptos)
+            });
+        }
+        //construir arreglo de rubros
+        self.detalle_rubros = function(concepto_orden_pago){
+          angular.forEach(concepto_orden_pago, function(i){
+            self.rubros.push(i.Concepto.Rubro)
+          })
+          // quitar repetidos
+          var hash = {};
+          self.rubros = self.rubros.filter(function(current) {
+            var exists = !hash[current.Id] || false;
+            hash[current.Id] = true;
+            return exists;
+          });
         }
       //
       },
