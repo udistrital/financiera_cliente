b776fa97a441d21fc3fc2e661ee0e939b67264f8
jotavargas <jotavargas89@gmail.com>
2017-04-17 22:50:08 -0500

    detalle cuentas contables orden pago proveedor

--- app/scripts/directives/cuentas_contables/movimientos_contables_op_detalle.js
+++ app/scripts/directives/cuentas_contables/movimientos_contables_op_detalle.js
@@ -7,58 +7,57 @@
  * # cuentasContables/movimientosContablesOpDetalle
  */
 angular.module('financieraClienteApp')
-  .directive('movimientosContablesOpDetalle', function (financieraRequest) {
+  .directive('movimientosContablesOpDetalle', function (financieraRequest, $timeout) {
     return {
       restrict: 'E',
       scope:{
-          codigodocumentoafectante:'='
+          codigodocumentoafectante:'=?'
         },
 
       templateUrl: 'views/directives/cuentas_contables/movimientos_contables_op_detalle.html',
       controller:function($scope){
         var self = this;
-
         self.gridOptions_movimientos = {
           enableRowSelection: true,
           enableRowHeaderSelection: false,
-
           columnDefs : [
-            {field: 'Id',                                                   visible : false},
-            {field: 'ConceptoCuentaContable.CuentaContable.Codigo',         displayName: 'Código Cuenta'},
-            {field: 'ConceptoCuentaContable.CuentaContable.Nombre',         displayName: 'Nombre Cuenta'},
-            {field: 'Debito',                                               displayName: 'Debito'},
-            {field: 'Credito',                                              displayName: 'Credito'},
-            {field: 'ConceptoCuentaContable.CuentaContable.Naturaleza',     displayName: 'Naturaleza'}
+            {field: 'Id',                                                     visible : false},
+            {field: 'ConceptoCuentaContable.CuentaContable.Codigo',           displayName: 'Código Cuenta'},
+            {field: 'ConceptoCuentaContable.CuentaContable.Nombre',           displayName: 'Nombre Cuenta'},
+            {field: 'Debito',                                                 displayName: 'Debito'},
+            {field: 'Credito',                                                displayName: 'Credito'},
+            {field: 'ConceptoCuentaContable.CuentaContable.Naturaleza',       displayName: 'Naturaleza'}
           ]
         };
-        // TipoDocumentoAfectante # 1 son las ordenes de pago
-        $scope.$watch('codigodocumentoafectante', function(){
-          console.log('AAAAAA');
-          console.log($scope.codigodocumentoafectante);
+        // refrescar
+        self.refresh = function() {
+          $scope.refresh = true;
+          $timeout(function() {
+            $scope.refresh = false;
+          }, 0);
+        };
 
+        $scope.$watch('codigodocumentoafectante', function(){
+          self.refresh();
           if($scope.codigodocumentoafectante != undefined){
-            console.log('BBBBBB');
             financieraRequest.get('movimiento_contable',
               $.param({
-                  query: "TipoDocumentoAfectante.Id:1,CodigoDocumentoAfectante:"+$scope.codigodocumentoafectante,
+                  query: "TipoDocumentoAfectante.Id:1,CodigoDocumentoAfectante:" + $scope.codigodocumentoafectante,
                   limit: 0,
               })).then(function(response) {
-                console.log(response.data)
-                self.gridOptions_movimientos = response.data;
+                self.gridOptions_movimientos.data = response.data;
             });
           }
         })
-        //
+
         self.gridOptions_movimientos.onRegisterApi = function(gridApi){
             //set gridApi on scope
             self.gridApi = gridApi;
             gridApi.selection.on.rowSelectionChanged($scope,function(row){
-              $scope.unidaejecutora = row.entity
+              $scope.movimientos = row.entity;
             });
           };
           self.gridOptions_movimientos.multiSelect = false;
-
-
       //
       },
       controllerAs:'d_movimientosContablesOpDetalle'
