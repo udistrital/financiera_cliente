a52a957cc9406a7bee606d27c30a1a93d0c283db
miguelramirez93 <darckzios@gmail.com>
2017-04-17 22:29:27 -0500

    se cambian aspectos de visualizacion de informacion en vistas CDP y RP (relacionadas)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -46,7 +46,6 @@ module.exports = function(config) {
       'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/sweetalert/dist/sweetalert.min.js',
-      'bower_components/es6-promise/es6-promise.js',
       'bower_components/sweetalert2/dist/sweetalert2.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
