0a97e3c1cf023dbb292eab1fdebb7846aff2d3f1
jotavargas <jotavargas89@gmail.com>
2017-04-21 13:00:05 -0500

    ajustes visuales del nav para detall orden de pago

--- app/scripts/directives/orden_pago/op_proveedor_detalle_pago.js
+++ app/scripts/directives/orden_pago/op_proveedor_detalle_pago.js
@@ -7,22 +7,24 @@
  * # ordenPago/opProveedorDetallePago
  */
 angular.module('financieraClienteApp')
-  .directive('opProveedorDetallePago', function (financieraRequest) {
+  .directive('opProveedorDetallePago', function(financieraRequest) {
     return {
       restrict: 'E',
-      scope:{
-          tipoordenpago:'=?',
-          iva: '=?',
-          valorbase: '=?'
-        },
+      scope: {
+        tipoordenpago: '=?',
+        iva: '=?',
+        valorbase: '=?'
+      },
 
       templateUrl: 'views/directives/orden_pago/op_proveedor_detalle_pago.html',
-      controller:function($scope){
+      controller: function($scope) {
         var self = this;
 
         //tipo_documentos
         financieraRequest.get('tipo_orden_pago',
-          $.param({limit:0})
+          $.param({
+            limit: 0
+          })
         ).then(function(response) {
           self.tipo_orden_pago_data = response.data;
         });
@@ -30,28 +32,28 @@ angular.module('financieraClienteApp')
         financieraRequest.get('iva',
           $.param({
             query: "CategoriaIva.Nombre:Gravados",
-            limit:0
+            limit: 0
           })
         ).then(function(response) {
           self.iva_data = response.data;
         });
         // valor bruto
-        self.get_valor_bruto  = function (valor_base, iva){
-          if(valor_base == null || valor_base == 0){
+        self.get_valor_bruto = function(valor_base, iva) {
+          if (valor_base == null || valor_base == 0) {
             self.ValorIva = 0;
-            self.ValorBruto =0;
-          }else if(iva == null || iva == 0) {
+            self.ValorBruto = 0;
+          } else if (iva == null || iva == 0) {
             self.ValorIva = 0;
-            self.ValorBruto =0;
-          }else{
+            self.ValorBruto = 0;
+          } else {
             self.Iva = parseInt(iva.Valor);
-            self.ValorIva = ( parseInt(valor_base) * ( parseInt(self.Iva)/100) );
+            self.ValorIva = (parseInt(valor_base) * (parseInt(self.Iva) / 100));
             self.ValorBruto = parseInt(valor_base) + parseInt(self.ValorIva);
           }
         }
 
 
       },
-      controllerAs:'d_opProveedorDetallePago'
+      controllerAs: 'd_opProveedorDetallePago'
     };
   });
