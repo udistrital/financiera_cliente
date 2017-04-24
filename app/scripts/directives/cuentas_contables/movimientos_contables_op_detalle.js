0e86cee077eebf0b797ab8886ec38fab5197d69f
jotavargas <jotavargas89@gmail.com>
2017-04-24 15:25:29 -0500

    organizar codigo

--- app/scripts/directives/cuentas_contables/movimientos_contables_op_detalle.js
+++ app/scripts/directives/cuentas_contables/movimientos_contables_op_detalle.js
@@ -7,26 +7,45 @@
  * # cuentasContables/movimientosContablesOpDetalle
  */
 angular.module('financieraClienteApp')
-  .directive('movimientosContablesOpDetalle', function (financieraRequest, $timeout, $translate) {
+  .directive('movimientosContablesOpDetalle', function(financieraRequest, $timeout, $translate) {
     return {
       restrict: 'E',
-      scope:{
-          codigodocumentoafectante:'=?'
-        },
+      scope: {
+        codigodocumentoafectante: '=?'
+      },
 
       templateUrl: 'views/directives/cuentas_contables/movimientos_contables_op_detalle.html',
-      controller:function($scope){
+      controller: function($scope) {
         var self = this;
         self.gridOptions_movimientos = {
           enableRowSelection: true,
           enableRowHeaderSelection: false,
-          columnDefs : [
-            {field: 'Id',                                                     visible : false},
-            {field: 'ConceptoCuentaContable.CuentaContable.Codigo',           displayName: $translate.instant('CODIGO')},
-            {field: 'ConceptoCuentaContable.CuentaContable.Nombre',           displayName: $translate.instant('CUENTA')},
-            {field: 'Debito',                                                 displayName: $translate.instant('DEBITO'), cellFilter: 'currency'},
-            {field: 'Credito',                                                displayName: $translate.instant('CREDITO'), cellFilter: 'currency'},
-            {field: 'ConceptoCuentaContable.CuentaContable.Naturaleza',       displayName: $translate.instant('NATURALEZA')}
+          columnDefs: [{
+              field: 'Id',
+              visible: false
+            },
+            {
+              field: 'ConceptoCuentaContable.CuentaContable.Codigo',
+              displayName: $translate.instant('CODIGO')
+            },
+            {
+              field: 'ConceptoCuentaContable.CuentaContable.Nombre',
+              displayName: $translate.instant('CUENTA')
+            },
+            {
+              field: 'Debito',
+              displayName: $translate.instant('DEBITO'),
+              cellFilter: 'currency'
+            },
+            {
+              field: 'Credito',
+              displayName: $translate.instant('CREDITO'),
+              cellFilter: 'currency'
+            },
+            {
+              field: 'ConceptoCuentaContable.CuentaContable.Naturaleza',
+              displayName: $translate.instant('NATURALEZA')
+            }
           ]
         };
         // refrescar
@@ -37,7 +56,7 @@ angular.module('financieraClienteApp')
           }, 0);
         };
         // operar_repetidos
-        self.totalizar_cuentas_repetidas = function(movimientos){
+        self.totalizar_cuentas_repetidas = function(movimientos) {
           self.retornar_movimientos = movimientos;
           // quitar repetidos
           var hash = {};
@@ -47,26 +66,26 @@ angular.module('financieraClienteApp')
             return exists;
           });
           //
-          $scope.counter  = {};
+          $scope.counter = {};
           // contar los repetidos
           movimientos.forEach(function(obj) {
-              var key = obj.ConceptoCuentaContable.CuentaContable.Codigo
-              $scope.counter[key] = ($scope.counter[key] || 0) + 1
+            var key = obj.ConceptoCuentaContable.CuentaContable.Codigo
+            $scope.counter[key] = ($scope.counter[key] || 0) + 1
           })
           // sumar
-          angular.forEach($scope.counter, function(value, key){
-            if(value > 1){
+          angular.forEach($scope.counter, function(value, key) {
+            if (value > 1) {
               var debito = 0;
               var credito = 0;
-              angular.forEach(movimientos, function(movimiento){
-                if(movimiento.ConceptoCuentaContable.CuentaContable.Codigo == key){
+              angular.forEach(movimientos, function(movimiento) {
+                if (movimiento.ConceptoCuentaContable.CuentaContable.Codigo == key) {
                   credito = credito + movimiento.Credito;
                   debito = debito + movimiento.Debito;
                 }
               })
               //asigno totales
-              angular.forEach(self.retornar_movimientos,function(mov_sin_repe){
-                if(mov_sin_repe.ConceptoCuentaContable.CuentaContable.Codigo == key){
+              angular.forEach(self.retornar_movimientos, function(mov_sin_repe) {
+                if (mov_sin_repe.ConceptoCuentaContable.CuentaContable.Codigo == key) {
                   mov_sin_repe.Credito = credito;
                   mov_sin_repe.Debito = debito;
                 }
@@ -77,32 +96,32 @@ angular.module('financieraClienteApp')
           return self.retornar_movimientos
         }
 
-        $scope.$watch('codigodocumentoafectante', function(){
+        $scope.$watch('codigodocumentoafectante', function() {
           self.refresh();
-          if($scope.codigodocumentoafectante != undefined){
+          if ($scope.codigodocumentoafectante != undefined) {
             financieraRequest.get('movimiento_contable',
               $.param({
-                  query: "TipoDocumentoAfectante.Id:1,CodigoDocumentoAfectante:" + $scope.codigodocumentoafectante,
-                  limit: 0,
+                query: "TipoDocumentoAfectante.Id:1,CodigoDocumentoAfectante:" + $scope.codigodocumentoafectante,
+                limit: 0,
               })).then(function(response) {
-                //self.gridOptions_movimientos.data = response.data
-                self.gridOptions_movimientos.data = self.totalizar_cuentas_repetidas(response.data)
-                $scope.gridHeight = self.gridOptions_movimientos.rowHeight * 2 + (self.gridOptions_movimientos.data.length * self.gridOptions_movimientos.rowHeight);
+              //self.gridOptions_movimientos.data = response.data
+              self.gridOptions_movimientos.data = self.totalizar_cuentas_repetidas(response.data)
+              $scope.gridHeight = self.gridOptions_movimientos.rowHeight * 2 + (self.gridOptions_movimientos.data.length * self.gridOptions_movimientos.rowHeight);
 
             });
           }
         })
 
-        self.gridOptions_movimientos.onRegisterApi = function(gridApi){
-            //set gridApi on scope
-            self.gridApi = gridApi;
-            gridApi.selection.on.rowSelectionChanged($scope,function(row){
-              $scope.movimientos = row.entity;
-            });
-          };
-          self.gridOptions_movimientos.multiSelect = false;
-      //
+        self.gridOptions_movimientos.onRegisterApi = function(gridApi) {
+          //set gridApi on scope
+          self.gridApi = gridApi;
+          gridApi.selection.on.rowSelectionChanged($scope, function(row) {
+            $scope.movimientos = row.entity;
+          });
+        };
+        self.gridOptions_movimientos.multiSelect = false;
+        //
       },
-      controllerAs:'d_movimientosContablesOpDetalle'
+      controllerAs: 'd_movimientosContablesOpDetalle'
     };
   });
