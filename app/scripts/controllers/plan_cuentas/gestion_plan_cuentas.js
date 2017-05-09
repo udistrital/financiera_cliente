dfc010b231f71de2b93ba4db3bc5ccaa08678681
SalcedoGeiner <geiner.salcedo.ppc0111@gmail.com>
2017-05-09 11:56:30 -0500

    se crea modulo para gestion del plan de cuentas

--- app/scripts/controllers/plan_cuentas/gestion_plan_cuentas.js
+++ app/scripts/controllers/plan_cuentas/gestion_plan_cuentas.js
@@ -8,10 +8,16 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('GestionPlanCuentasCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('GestionPlanCuentasCtrl', function (financieraRequest) {
+    var self=this;
+
+    self.cargar_plan_maestro = function() {
+      financieraRequest.get("plan_cuentas", $.param({
+        query: "PlanMaestro:" + true
+      })).then(function(response) {
+        self.plan_maestro = response.data[0]; //Se carga data devuelta por el servicio en la variable del controlador plan_maestro
+      });
+    };
+
+    self.cargar_plan_maestro();
   });
