7193da86f1aaf222f296f0911c6b507e089ed403
jotavargas <jotavargas89@gmail.com>
2017-05-23 14:31:20 -0500

    #12116 solicitud de vigencia con el servicio

--- app/scripts/controllers/orden_pago/planta/op_planta_crear.js
+++ app/scripts/controllers/orden_pago/planta/op_planta_crear.js
@@ -9,5 +9,14 @@
  */
 angular.module('financieraClienteApp')
   .controller('OpPlantaCrearCtrl', function ($scope, financieraRequest, $window, $translate) {
+    var self = this;
     $scope.a = true;
+    // obtener vigencia
+    financieraRequest.get("orden_pago/FechaActual/2006")  //formato de entrada  https://golang.org/src/time/format.go
+      .then(function(data) { //error con el success
+        self.fecha = data;
+        console.log(self.fecha.data)
+      })
+
+
   });
