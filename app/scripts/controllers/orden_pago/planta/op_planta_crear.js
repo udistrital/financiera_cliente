0b94f41c120dc5749b23339112c2f2fddc9ff09e
jotavargas <jotavargas89@gmail.com>
2017-05-24 16:14:58 -0500

    #12116 ajustes para busqueda de nomina

--- app/scripts/controllers/orden_pago/planta/op_planta_crear.js
+++ app/scripts/controllers/orden_pago/planta/op_planta_crear.js
@@ -11,11 +11,12 @@ angular.module('financieraClienteApp')
   .controller('OpPlantaCrearCtrl', function ($scope, financieraRequest, $window, $translate) {
     var self = this;
     $scope.a = true;
+    self.OrdenPago = {};
+    self.OrdenPagoConsulta = {};
     // obtener vigencia
     financieraRequest.get("orden_pago/FechaActual/2006")  //formato de entrada  https://golang.org/src/time/format.go
       .then(function(data) { //error con el success
-        self.fecha = data;
-        console.log(self.fecha.data)
+        self.OrdenPago.Vigencia = data.data;
       })
 
 
