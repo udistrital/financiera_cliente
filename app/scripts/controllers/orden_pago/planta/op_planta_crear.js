70aae492220c70a8a76fd0d016a6e2cc4e3614d0
jotavargas <jotavargas89@gmail.com>
2017-05-30 10:13:57 -0500

    #12116 datos de entrada orden pago planta

--- app/scripts/controllers/orden_pago/planta/op_planta_crear.js
+++ app/scripts/controllers/orden_pago/planta/op_planta_crear.js
@@ -8,18 +8,22 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('OpPlantaCrearCtrl', function ($scope, financieraRequest, $window, $translate) {
+  .controller('OpPlantaCrearCtrl', function($scope, financieraRequest, $window, $translate) {
     var self = this;
-    $scope.a = true;
+    self.PestanaAbierta = true;
     self.OrdenPago = {};
+    self.OrdenPagoConsulta = {};
+    self.OrdenPagoConsulta.Proveedor = { Id: 469 };
     self.dataLiquidacionConsulta = {};
     self.dataliquidacion = {};
     // obtener vigencia
-    financieraRequest.get("orden_pago/FechaActual/2006")  //formato de entrada  https://golang.org/src/time/format.go
+    financieraRequest.get("orden_pago/FechaActual/2006") //formato de entrada  https://golang.org/src/time/format.go
       .then(function(data) { //error con el success
         self.OrdenPago.Vigencia = data.data;
         self.dataLiquidacionConsulta.Vigencia = self.OrdenPago.Vigencia;
       })
-
-
+    // Funciones
+    self.addOpPlantaCrear = function(){
+      console.log(self.OrdenPago)
+    }
   });
