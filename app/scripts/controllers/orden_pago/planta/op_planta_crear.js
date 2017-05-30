9e3509c6e1ba4629e4e512285b39c801fab8b2d9
jotavargas <jotavargas89@gmail.com>
2017-05-30 10:58:08 -0500

    #12116 se agrega validaciones en op planta

--- app/scripts/controllers/orden_pago/planta/op_planta_crear.js
+++ app/scripts/controllers/orden_pago/planta/op_planta_crear.js
@@ -13,7 +13,7 @@ angular.module('financieraClienteApp')
     self.PestanaAbierta = true;
     self.OrdenPago = {};
     self.OrdenPagoConsulta = {};
-    self.OrdenPagoConsulta.Proveedor = { Id: 469 };
+    self.OrdenPagoConsulta.Proveedor = {Id: 469};
     self.dataLiquidacionConsulta = {};
     self.dataliquidacion = {};
     // obtener vigencia
@@ -22,8 +22,49 @@ angular.module('financieraClienteApp')
         self.OrdenPago.Vigencia = data.data;
         self.dataLiquidacionConsulta.Vigencia = self.OrdenPago.Vigencia;
       })
+    // ***************
     // Funciones
-    self.addOpPlantaCrear = function(){
+    // ***************
+    self.validar_campos = function() {
+      self.MensajesAlerta = '';
+      if (self.OrdenPago.UnidadEjecutora == undefined) {
+        self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_UNIDAD') + "</li>"
+      }
+      if (self.OrdenPago.RegistroPresupuestal == undefined) {
+        self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_REGISTRO') + "</li>"
+      }
+      if (self.OrdenPago.Liquidacion == undefined) {
+        self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_LIQUIDACION') + "</li>"
+      }
+      // Operar
+      if (self.MensajesAlerta == undefined || self.MensajesAlerta.length == 0) {
+        // insertc
+        console.log("Insertar DATA");
+        /*financieraRequest.post("orden_pago/RegistrarOpProveedor", self.dataOrdenPagoInsert)
+          .then(function(data) { //error con el success
+            self.resultado = data;
+            //mensaje
+            swal({
+              title: 'Registro Exitoso',
+              text: 'Orden de Pago Proveedo Registrado Exitosamente con Consecutivo No. ' + self.resultado.data,
+              type: 'success',
+            }).then(function() {
+              $window.location.href = '#/orden_pago/ver_todos';
+            })
+            //
+          })*/
+      } else {
+        // mesnajes de error
+        swal({
+          title: 'Error!',
+          html: '<ol align="left">' + self.MensajesAlerta + '</ol>',
+          type: 'error'
+        })
+      }
+    }
+    //
+    self.addOpPlantaCrear = function() {
       console.log(self.OrdenPago)
+      self.validar_campos()
     }
   });
