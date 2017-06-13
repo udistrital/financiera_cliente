96c89e5eccc9675a1b2a47945884a7ae765d880f
Fabian D. Sanchez L <fadarsaleeing@gmail.com>
2017-06-13 17:20:31 -0500

    controlador y vista para el envio de formulario de solicitud de un avance

--- app/scripts/controllers/tesoreria/avances/solicitud/solicitud_avance.js
+++ app/scripts/controllers/tesoreria/avances/solicitud/solicitud_avance.js
@@ -8,14 +8,14 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('SolicitudAvanceCtrl', function ($scope, modelsRequest,financieraRequest) {
+  .controller('SolicitudAvanceCtrl', function($scope, modelsRequest, financieraRequest) {
     var ctrl = this;
     $scope.info_terceros = true;
     $scope.info_desc_avances = true;
     $scope.info_detalle_avances = true;
     ctrl.tipos_avance = [];
 
-    ctrl.get_tipos_avance = function(){
+    ctrl.get_tipos_avance = function() {
       financieraRequest.get("tipo_avance", $.param({
           query: "Estado:" + 'A',
           limit: -1,
@@ -28,18 +28,48 @@ angular.module('financieraClienteApp')
         });
     };
 
-    ctrl.get_terceros = function(){
+    ctrl.get_terceros = function() {
       modelsRequest.get("terceros_completo")
-      .then(function(response){
-        if (ctrl.documento === response.data.documento) {
-          ctrl.terceros = response.data;
-          ctrl.get_tipos_avance();
-        }else{
-          ctrl.documento = "";
-          ctrl.terceros = [];
-        }
-        console.log(ctrl.terceros);
-      });
+        .then(function(response) {
+          if (ctrl.documento === response.data.documento) {
+            ctrl.terceros = response.data;
+            ctrl.get_tipos_avance();
+          } else {
+            ctrl.documento = "";
+            ctrl.terceros = [];
+          }
+          console.log(ctrl.terceros);
+        });
+    };
+
+
+
+    ctrl.enviar = function(){
+      var Solicitud = {};
+      var TipoAvance = {};
+      var SolicitudAvance = {};
+      Solicitud.IdBeneficiario = ctrl.documento;
+      Solicitud.Objetivo = ctrl.objetivo;
+      Solicitud.Justificacion = ctrl.justificacion;
+      Solicitud.CodigoDependencia = ctrl.terceros.dependencia.id;
+      Solicitud.Dependencia = ctrl.terceros.dependencia.dependencia;
+      Solicitud.CodigoFacultad = ctrl.terceros.proyecto_curricular.facultad.id;
+      Solicitud.Facultad = ctrl.terceros.proyecto_curricular.facultad.Facultad;
+      Solicitud.CodigoProyectoCur = ctrl.terceros.proyecto_curricular.CodigoProyectoCurricular.id;
+      Solicitud.ProyectoCurricular = ctrl.terceros.proyecto_curricular.CodigoProyectoCurricular.ProyectoCurricular;
+
+      Solicitud.CodigoConvenio = ctrl.codigo_convenio;
+      Solicitud.Convenio = ctrl.nombre_convenio;
+      Solicitud.CodigoProyectoInv = ctrl.codigo_proyecto_inv;
+      Solicitud.ProyectoInv = ctrl.nombre_proyecto_inv;
+
+      TipoAvance.IdTipo = parseInt(ctrl.tipo_avance_select);
+      TipoAvance.Descripcion = ctrl.descripcion;
+      TipoAvance.Valor = parseFloat(ctrl.valor_avance);
+
+      SolicitudAvance.Solicitud = Solicitud;
+      SolicitudAvance.TipoAvance = TipoAvance;
+      console.log(SolicitudAvance);
     };
 
 
