481d10413039e7557b93b86e709a810d27de5482
Miguel Ramirez <darckzios@gmail.com>
2017-06-16 09:04:28 -0500

    prueba arreglo consulta ingresos

--- app/scripts/controllers/ingresos/ingreso_consulta.js
+++ app/scripts/controllers/ingresos/ingreso_consulta.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 /**
@@ -211,6 +212,7 @@ self.Rechazar = function() {
                 swal('', $translate.instant(response.data.Code) + response.data.Body.Consecutivo, response.data.Type).then(function() {
 
                   self.cargarIngresos();
+
                 });
               }
 
