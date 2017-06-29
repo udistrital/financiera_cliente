937b94699d787f320e75589bbaa60792755fedb7
Fabian D. Sanchez L <fadarsaleeing@gmail.com>
2017-06-29 12:46:54 -0500

    documentación de código de fabrica de notificación

--- app/scripts/controllers/notificaciones.js
+++ app/scripts/controllers/notificaciones.js
@@ -15,6 +15,8 @@
 angular.module('financieraClienteApp')
 
   .controller('NotificacionesCtrl', function($scope, notificacion) {
+    //path de imagenes de notificacion, esto será temporal
     $scope.imagePath = 'images/yeoman.png';
+    //Paso de parametro a la vista por scope para uso desde vista de notificacion
     $scope.notificacion = notificacion;
   });
