d3971c7625b551b10e248981b641294fe1d7f9b7
Fabian D. Sanchez L <fadarsaleeing@gmail.com>
2017-06-29 12:44:48 -0500

    documentación de código de fabrica de notificación

--- app/scripts/controllers/notificaciones.js
+++ app/scripts/controllers/notificaciones.js
@@ -3,11 +3,17 @@
 /**
  * @ngdoc function
  * @name financieraClienteApp.controller:NotificacionesCtrl
+ * @alias notificaciones
+ * @requires $scope
+ * @requires financieraNotificacion.service:notificacion
+ * @param {service} notificacion Servicio para el API de financiera {@link financieraNotificacion.service:notificacion}
  * @description
  * # NotificacionesCtrl
- * Controller of the financieraClienteApp
+ * Controller de vista de notificaciones
  */
+
 angular.module('financieraClienteApp')
+
   .controller('NotificacionesCtrl', function($scope, notificacion) {
     $scope.imagePath = 'images/yeoman.png';
     $scope.notificacion = notificacion;
