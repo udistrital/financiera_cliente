7815c74289c2123ca05ec16dafb5b3732af7298b
jotavargas <jotavargas89@gmail.com>
2017-06-30 10:15:57 -0500

    #13365 secciones de la op nomina

--- app/scripts/controllers/orden_pago/seguridad_social/op_seguridad_social_crear.js
+++ app/scripts/controllers/orden_pago/seguridad_social/op_seguridad_social_crear.js
@@ -8,8 +8,19 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('OpSeguridadSocialCrearCtrl', function () {
+  .controller('OpSeguridadSocialCrearCtrl', function ($scope, financieraRequest, $window, $translate, financieraMidRequest, titanRequest, $http) {
     var self = this;
     self.PestanaAbierta = true;
     self.OrdenPago = {};
+
+    // obtener vigencia
+    financieraRequest.get("orden_pago/FechaActual/2006") //formato de entrada  https://golang.org/src/time/format.go
+      .then(function(data) { //error con el success
+        self.OrdenPago.Vigencia = parseInt(data.data);
+        self.dataLiquidacionConsulta.Vigencia = self.OrdenPago.Vigencia;
+      })
+    // ***************
+    // Funciones
+    // ***************
+
   });
