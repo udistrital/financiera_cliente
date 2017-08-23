da0e0fc28fdf6500ba5ebc5af6899e63298e520a
Fabian D. Sanchez L <fadarsaleeing@gmail.com>
2017-08-23 14:53:34 -0500

    recibiendo nodos y aristas en directiva de procesos

--- app/scripts/controllers/tesoreria/avances/solicitud/lista_solicitud.js
+++ app/scripts/controllers/tesoreria/avances/solicitud/lista_solicitud.js
@@ -169,6 +169,19 @@ angular.module('financieraClienteApp')
                     $('#modal_ver').modal('show');
                     break;
                 case "estado":
+                    $scope.estados = [
+                        { id: 1, label: 'Node 1' },
+                        { id: 2, label: 'Node 2' },
+                        { id: 3, label: 'Node 3' },
+                        { id: 4, label: 'Node 4' },
+                        { id: 5, label: 'Node 5' }
+                    ];
+                    $scope.aristas = [
+                        { from: 1, to: 3 },
+                        { from: 1, to: 2 },
+                        { from: 2, to: 4 },
+                        { from: 2, to: 5 }
+                    ];
                     $('#modal_estado').modal('show');
                     break;
                 case "validar":
--- app/scripts/directives/proceso.js
+++ app/scripts/directives/proceso.js
@@ -10,44 +10,34 @@ angular.module('financieraClienteApp')
     .directive('proceso', function() {
         return {
             restrict: 'E',
-            /*scope:{
-                var:'='
-              },
-            */
+            scope: {
+                nodes: '=?',
+                edges: '=?'
+            },
+
             templateUrl: 'views/directives/proceso.html',
-            controller: function() {
-                var nodes = new vis.DataSet([
-                    { id: 1, label: 'Node 1' },
-                    { id: 2, label: 'Node 2' },
-                    { id: 3, label: 'Node 3' },
-                    { id: 4, label: 'Node 4' },
-                    { id: 5, label: 'Node 5' }
-                ]);
+            controller: function($scope) {
+                var nodes = {};
+                var edges = {};
+                var network = {};
+                $scope.$watch('nodes', function() {
+                    nodes = new vis.DataSet($scope.nodes);
+                    edges = new vis.DataSet($scope.edges);
+                    var container = document.getElementById('mynetwork');
+                    // provide the data in the vis format
+                    var data = {
+                        nodes: nodes,
+                        edges: edges
+                    };
+                    var options = {
+                        clusterNodeProperties: {
+                            allowSingleNodeCluster: true
+                        }
+                    };
+                    // initialize your network!
+                    network = new vis.Network(container, data, options);
+                });
 
-                // create an array with edges
-                var edges = new vis.DataSet([
-                    { from: 1, to: 3 },
-                    { from: 1, to: 2 },
-                    { from: 2, to: 4 },
-                    { from: 2, to: 5 }
-                ]);
-
-                // create a network
-                var container = document.getElementById('mynetwork');
-
-                // provide the data in the vis format
-                var data = {
-                    nodes: nodes,
-                    edges: edges
-                };
-                var options = {
-                    clusterNodeProperties: {
-                        allowSingleNodeCluster: true
-                    }
-                };
-
-                // initialize your network!
-                var network = new vis.Network(container, data, options);
             },
             controllerAs: 'd_proceso'
         };
