8adf864cefeeb143d649aef5a0924f86f78eb378
jotavargas <jotavargas89@gmail.com>
2017-09-08 17:06:10 -0500

    vista de giros con total

--- app/scripts/directives/orden_pago/op_multi_select.js
+++ app/scripts/directives/orden_pago/op_multi_select.js
@@ -7,7 +7,7 @@
  * # ordenPago/opMultiSelect
  */
 angular.module('financieraClienteApp')
-  .directive('opMultiSelect', function (financieraRequest, $timeout, $translate) {
+  .directive('opMultiSelect', function (financieraRequest, agoraRequest, $timeout, $translate, uiGridConstants) {
     return {
       restrict: 'E',
       scope:{
@@ -19,6 +19,7 @@ angular.module('financieraClienteApp')
         var self = this;
         //
         self.gridOptions_op = {
+          showColumnFooter: true,
           enableRowSelection: true,
           enableRowHeaderSelection: true,
 
@@ -39,13 +40,13 @@ angular.module('financieraClienteApp')
             {
               field: 'Consecutivo',
               displayName: $translate.instant('CODIGO'),
-              width: '8%',
+              width: '7%',
               cellClass: 'input_center'
             },
             {
               field: 'Vigencia',
               displayName: $translate.instant('VIGENCIA'),
-              width: '8%',
+              width: '7%',
               cellClass: 'input_center'
             },
             {
@@ -53,23 +54,53 @@ angular.module('financieraClienteApp')
               displayName: $translate.instant('FECHA_CREACION'),
               cellClass: 'input_center',
               cellFilter: "date:'yyyy-MM-dd'",
+              width: '8%',
             },
             {
               field: 'RegistroPresupuestal.NumeroRegistroPresupuestal',
               displayName: $translate.instant('NO_CRP'),
-              width: '8%',
+              width: '7%',
               cellClass: 'input_center'
             },
             {
               field: 'TipoOrdenPago.Nombre',
+              width: '7%',
               displayName: $translate.instant('TIPO_DOCUMENTO')
             },
             {
               field: 'Nomina',
+              width: '10%',
               displayName: $translate.instant('NOMINA')
             },
             {
+              field: 'Proveedor.Tipopersona',
+              width: '10%',
+              displayName: $translate.instant('TIPO_PERSONA')
+            },
+            {
+              field: 'Proveedor.NomProveedor',
+              displayName: $translate.instant('NOMBRE')
+            },
+            {
+              field: 'Proveedor.NumDocumento',
+              width: '10%',
+              cellClass: 'input_center',
+              displayName: $translate.instant('NO_DOCUMENTO')
+            },
+            {
+              field: 'ValorTotal',
+              width: '10%',
+              cellFilter: 'currency',
+              cellClass: 'input_right',
+              displayName: $translate.instant('VALOR'),
+
+              aggregationType: uiGridConstants.aggregationTypes.sum,
+              footerCellFilter: 'currency',
+              footerCellClass: 'input_right'
+            },
+            {
               field: 'OrdenPagoEstadoOrdenPago[0].EstadoOrdenPago.Nombre',
+              width: '7%',
               displayName: $translate.instant('ESTADO')
             },
           ]
@@ -92,6 +123,21 @@ angular.module('financieraClienteApp')
               })).then(function(response) {
               self.refresh();
               self.gridOptions_op.data = response.data;
+              // data proveedor
+              angular.forEach(self.gridOptions_op.data, function(iterador){
+                agoraRequest.get('informacion_proveedor',
+                  $.param({
+                    query: "Id:" + iterador.RegistroPresupuestal.Beneficiario,
+                  })
+                ).then(function(response) {
+                  iterador.Proveedor = response.data[0];
+                });
+                financieraRequest.post('orden_pago/ValorTotal/' + iterador.Id ,
+                ).then(function(response) {
+                  iterador.ValorTotal = response.data;
+                });
+              })
+              // data proveedor
             });
           }
         },true)
