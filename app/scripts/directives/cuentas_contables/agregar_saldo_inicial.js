56fce14009a43dfbac0b48fe51a21e2ac438a216
Geiner <you@example.com>
2017-09-12 11:16:33 -0500

    prototipos saldos iniciales

--- app/scripts/directives/cuentas_contables/agregar_saldo_inicial.js
+++ app/scripts/directives/cuentas_contables/agregar_saldo_inicial.js
@@ -7,7 +7,7 @@
  * # cuentasContables/agregarSaldoInicial
  */
 angular.module('financieraClienteApp')
-  .directive('agregarSaldoInicial', function() {
+  .directive('agregarSaldoInicial', function(financieraRequest) {
     return {
       restrict: 'E',
       /*scope:{
@@ -15,62 +15,19 @@ angular.module('financieraClienteApp')
         },
       */
       templateUrl: 'views/directives/cuentas_contables/agregar_saldo_inicial.html',
-      controller: function() {
-        var ctrl = this;
-        $scope.treeOptions = {
-          nodeChildren: "children",
-          dirSelectable: true,
-          injectClasses: {
-            ul: "a1",
-            li: "a2",
-            liSelected: "a7",
-            iExpanded: "a3",
-            iCollapsed: "a4",
-            iLeaf: "a5",
-            label: "a6",
-            labelSelected: "a8"
-          }
-        }
-        $scope.dataForTheTree = [{
-            "name": "Joe",
-            "age": "21",
-            "children": [{
-                "name": "Smith",
-                "age": "42",
-                "children": []
-              },
-              {
-                "name": "Gary",
-                "age": "21",
-                "children": [{
-                  "name": "Jenifer",
-                  "age": "23",
-                  "children": [{
-                      "name": "Dani",
-                      "age": "32",
-                      "children": []
-                    },
-                    {
-                      "name": "Max",
-                      "age": "34",
-                      "children": []
-                    }
-                  ]
-                }]
-              }
-            ]
-          },
-          {
-            "name": "Albert",
-            "age": "33",
-            "children": []
-          },
-          {
-            "name": "Ron",
-            "age": "29",
-            "children": []
-          }
-        ];
+      controller: function($scope) {
+        var self = this;
+        self.cargar_plan_maestro = function() {
+          console.log("enenene");
+          financieraRequest.get("plan_cuentas", $.param({
+            query: "PlanMaestro:" + true
+          })).then(function(response) {
+            self.plan_maestro = response.data[0]; //Se carga data devuelta por el servicio en la variable del controlador plan_maestro
+            console.log(self.plan_maestro);
+          });
+        };
+        self.cargar_plan_maestro();
+
       },
       controllerAs: 'd_agregarSaldoInicial'
     };
