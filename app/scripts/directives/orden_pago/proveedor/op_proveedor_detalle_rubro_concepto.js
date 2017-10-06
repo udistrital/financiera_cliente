9667076a1e0f5fd088db8dd44fc5092425395198
jotavargas <jotavargas89@gmail.com>
2017-10-06 13:20:55 -0500

    ajustes en vista de cosulta de op proveedor

--- app/scripts/directives/orden_pago/proveedor/op_proveedor_detalle_rubro_concepto.js
+++ app/scripts/directives/orden_pago/proveedor/op_proveedor_detalle_rubro_concepto.js
@@ -20,81 +20,8 @@ angular.module('financieraClienteApp')
         var self = this;
 
         self.gridOptions_rubros = {
-          enableRowSelection: false,
-          multiSelect: false,
-          enableRowHeaderSelection: false,
-
-          paginationPageSizes: [10, 50, 100],
-          paginationPageSize: null,
-
-          enableFiltering: true,
-          enableSelectAll: true,
-          enableHorizontalScrollbar: 0,
-          enableVerticalScrollbar: 0,
-          //minRowsToShow: 10,
-          useExternalPagination: false,
-
-          // inicio sub tabla
           expandableRowTemplate: 'expandableRowUpc.html',
           expandableRowHeight: 100,
-          onRegisterApi: function(gridApi) {
-            gridApi.expandable.on.rowExpandedStateChanged($scope, function(row) {
-              if (row.isExpanded) {
-                row.entity.subGridOptions = {
-                  multiSelect: true,
-                  columnDefs: [{
-                      field: 'Id',
-                      visible: false,
-                      enableCellEdit: false
-                    },
-                    {
-                      field: 'Codigo',
-                      displayName: $translate.instant('CODIGO') + ' ' + $translate.instant('CONCEPTO'),
-                      enableCellEdit: false,
-                      width: '10%',
-                      cellClass: 'input_center'
-                    },
-                    {
-                      field: 'Nombre',
-                      displayName: $translate.instant('NOMBRE'),
-                      enableCellEdit: false
-                    },
-                    {
-                      field: 'Descripcion',
-                      displayName: $translate.instant('DESCRIPCION'),
-                      enableCellEdit: false
-                    },
-                    {
-                      field: 'TipoConcepto.Nombre',
-                      displayName: $translate.instant('TIPO'),
-                      enableCellEdit: false,
-                      width: '10%',
-                    },
-                  ],
-                  onRegisterApi: function(gridApi) {
-                    self.gridApi = gridApi;
-                    gridApi.selection.on.rowSelectionChanged(gridApi.grid.appScope, function(row2) {
-                      $scope.outputconceptos = self.gridApi.selection.getSelectedRows();
-                    });
-                  }
-                };
-                //
-                financieraRequest.get('concepto',
-                  $.param({
-                    query: "Rubro.Id:" + row.entity.DisponibilidadApropiacion.Apropiacion.Rubro.Id,
-                    limit: 0
-                  })
-                ).then(function(response) {
-                  row.entity.subGridOptions.data = response.data;
-                  //asociar RegistroPresupuestalDisponibilidadApropiacion
-                  angular.forEach(row.entity.subGridOptions.data, function(subGridData) {
-                    subGridData.RegistroPresupuestalDisponibilidadApropiacion = row.entity.Id
-                  });
-                });
-              } // if
-            });
-          },
-          // fin sub tabla
           columnDefs: [{
               field: 'RegistroPresupuestalDisponibilidadApropiacion.DisponibilidadApropiacion.Apropiacion.Rubro.Id',
               visible: false
@@ -142,7 +69,6 @@ angular.module('financieraClienteApp')
                 limit: 0
               })
             ).then(function(response) {
-              console.log(response.data);
               //quitar repetidos
               var hash = {};
               response.data = response.data.filter(function(current) {
@@ -150,16 +76,63 @@ angular.module('financieraClienteApp')
                 hash[current.RegistroPresupuestalDisponibilidadApropiacion.Id] = true;
                 return exists;
               });
-              console.log("sinRepeti");
-              console.log(response.data);
               //fin quitar repetidos
               self.gridOptions_rubros.data = response.data;
+              // recorrer para subgrip
+              angular.forEach(self.gridOptions_rubros.data, function(iterador) {
+                iterador.subGridOptions = {
+                  columnDefs: [{
+                      field: 'Concepto.Id',
+                      visible: false,
+                      enableCellEdit: false
+                    },
+                    {
+                      field: 'Concepto.Codigo',
+                      displayName: $translate.instant('CODIGO') + ' ' + $translate.instant('CONCEPTO'),
+                      enableCellEdit: false,
+                      width: '10%',
+                      cellClass: 'input_center'
+                    },
+                    {
+                      field: 'Concepto.Nombre',
+                      displayName: $translate.instant('NOMBRE'),
+                      enableCellEdit: false
+                    },
+                    {
+                      field: 'Concepto.Descripcion',
+                      displayName: $translate.instant('DESCRIPCION'),
+                      enableCellEdit: false
+                    },
+                    {
+                      field: 'Concepto.TipoConcepto.Nombre',
+                      displayName: $translate.instant('TIPO'),
+                      enableCellEdit: false,
+                      width: '10%',
+                    },
+                    {
+                      field: 'Valor',
+                      displayName: $translate.instant('AFECTACION'),
+                      enableCellEdit: false,
+                      width: '10%',
+                      cellFilter: 'currency',
+                      cellClass: 'input_right'
+                    },
+                  ]
+                };
+                //
+                financieraRequest.get('concepto_orden_pago',
+                  $.param({
+                    query: "OrdenDePago.Id:" + $scope.inputordenpagoid + ",RegistroPresupuestalDisponibilidadApropiacion.Id:" + iterador.RegistroPresupuestalDisponibilidadApropiacion.Id,
+                    limit: 0
+                  })
+                ).then(function(response) {
+                  iterador.subGridOptions.data = response.data;
+                });
+              }) //recorrer para subgrip
             });
           }
         })
-
-
-        //fin
+      //fin
       },
       controllerAs: 'd_opProveedorDetalleRubroConcepto'
     };
