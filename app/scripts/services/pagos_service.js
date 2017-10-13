3883b8ac4dc527f6e846f510d00401ae32adcf36
miguelramirez93 <darckzios@gmail.com>
2017-10-13 17:05:12 -0500

    #16994# se agrega consumo del servicio de wso2 de ingresos desde las bases de datos de academica, se agregan filtros por rangos de fechas faltantes, falta por definir consumo de los codigos de facultades y conceptos homologados desde este servicio, a demas el servicio recibe formatos de fechas diferentes a los manejados en kronos.

--- app/scripts/services/pagos_service.js
+++ app/scripts/services/pagos_service.js
@@ -11,12 +11,12 @@
    .factory('pagosRequest', function ($http) {
      // Service logic
      // ...
-     var path = "http://10.20.0.127/urano/index.php?data=B-7djBQWvIdLAEEycbH1n6e-3dACi5eLUOb63vMYhGo7UeM3zDHx3qb-XtnCmqd77icefAdke5B9RO4U1kMew1mK7rfB0yxyQQ-RBU-dLhA3LwBbPFFZkPQvvRhkJ5n_";
+     var path = "http://jbpm.udistritaloas.edu.co:8280/services/academicaProxyService/ingresos_concepto/";
      // Public API here
      return {
 
-       get: function (parametros) {
-         return $http.get(path, {params: parametros});
+       get: function (parametros,headers) {
+         return $http.get(path+parametros,headers);
 
        },
        post: function (tabla,elemento) {
