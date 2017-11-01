2c04b630599c2074fc4d452e9dd62646c4fcad55
Fabian León <fadarsaleeing@gmail.com>
2017-11-01 14:58:41 -0500

    #17490# Construcción del archivo de configuración y ubicación de variables en todos los servicios

--- app/scripts/services/token.js
+++ app/scripts/services/token.js
@@ -39,64 +39,24 @@ req.onreadystatechange = function(e) {
 };
 
 angular.module('financieraClienteApp')
-    .factory('token_service', function($location, $http, $localStorage) {
+    .factory('token_service', function($location, $http, $localStorage, CONF) {
         var service = {
             local: $localStorage.$default(params),
             //session: $sessionStorage.default(params),
             header: null,
             token: null,
-            //Configuracion de parametros identificacion unica oas-wso2
-            /*
-                config: {
-                    AUTORIZATION_URL: "https://wso2.udistritaloas.edu.co:9443/oauth2/authorize",
-                    CLIENTE_ID: "erUwZgne4K0FusAGYYTMDFBe61Ia",
-                    REDIRECT_URL:  "http://10.20.0.254/kronos",
-                    RESPONSE_TYPE: "id_token token",
-                    SCOPE: "openid profile email",
-                    BUTTON_CLASS: "btn btn-warning btn-sm",
-                    SIGN_OUT_URL: "https://wso2.udistritaloas.edu.co:9443/oidc/logout",
-                    SIGN_OUT_REDIRECT_URL:  "http://10.20.0.254/kronos",
-                    SIGN_OUT_APPEND_TOKEN: "true"
-                },
-            */
-            //Configuracion wso2 local
 
             config: {
-                AUTORIZATION_URL: "https://10.20.0.162:9443/oauth2/authorize",
-                CLIENTE_ID: "MWxzi3GKKMVj0RDitf4z5xWYVe0a",
-                REDIRECT_URL: "http://10.20.0.254/kronos/",
-                RESPONSE_TYPE: "id_token token",
-                SCOPE: "openid email",
-                BUTTON_CLASS: "btn btn-warning btn-sm",
-                SIGN_OUT_URL: "https://10.20.0.162:9443/oidc/logout",
-                SIGN_OUT_REDIRECT_URL: "http://10.20.0.254/kronos/",
-                SIGN_OUT_APPEND_TOKEN: "true"
+                AUTORIZATION_URL: CONF.GENERAL.TOKEN.AUTORIZATION_URL,
+                CLIENTE_ID: CONF.GENERAL.TOKEN.CLIENTE_ID,
+                REDIRECT_URL: CONF.GENERAL.TOKEN.REDIRECT_URL,
+                RESPONSE_TYPE: CONF.GENERAL.TOKEN.RESPONSE_TYPE,
+                SCOPE: CONF.GENERAL.TOKEN.SCOPE,
+                BUTTON_CLASS: CONF.GENERAL.TOKEN.BUTTON_CLASS,
+                SIGN_OUT_URL: CONF.GENERAL.TOKEN.SIGN_OUT_URL,
+                SIGN_OUT_REDIRECT_URL: CONF.GENERAL.TOKEN.SIGN_OUT_REDIRECT_URL,
+                SIGN_OUT_APPEND_TOKEN: CONF.GENERAL.TOKEN.SIGN_OUT_APPEND_TOKEN
             },
-
-            //Configuracion wso2 local
-            /*
-                  config: {
-                    AUTORIZATION_URL: "https://10.20.0.162:9443/oauth2/authorize",
-                    CLIENTE_ID: "NCM5qVWim6MeTGB4Ag4lyLBOlv0a",
-                    REDIRECT_URL:  "http://localhost:9000/",
-                    RESPONSE_TYPE: "id_token token",
-                    SCOPE: "openid email",
-                    BUTTON_CLASS: "btn btn-warning btn-sm",
-                    SIGN_OUT_URL: "https://10.20.0.162:9443/oidc/logout",
-                    SIGN_OUT_REDIRECT_URL:  "http://localhost:9000/",
-                    SIGN_OUT_APPEND_TOKEN: "true"
-                  },
-            */
-            //Configuracion de parametros oidc unica google
-            /*
-            config: {
-              AUTORIZATION_URL: "https://accounts.google.com/o/oauth2/v2/auth",
-              CLIENTE_ID: "794841744026-6p2i7lmiho204r4li2bb1ektd7j9dbd4.apps.googleusercontent.com",
-              REDIRECT_URL: "https://fabianleon.github.io/app",
-              RESPONSE_TYPE: "id_token token",
-              SCOPE: "openid profile email",
-              BUTTON_CLASS: "btn btn-outline btn-primary btn-sm"
-            },*/
 
             live_token: function() {
                 if (typeof service.local.id_token === 'undefined' || service.local.id_token === null) {
