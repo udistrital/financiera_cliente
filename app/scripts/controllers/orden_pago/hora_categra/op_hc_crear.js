eae026c246eefefce55d126996ae3afbaacde708
jotavargas <jotavargas89@gmail.com>
2017-11-08 17:22:52 -0500

    #17125# @jjvargass control de campos obligatorios

--- app/scripts/controllers/orden_pago/hora_categra/op_hc_crear.js
+++ app/scripts/controllers/orden_pago/hora_categra/op_hc_crear.js
@@ -16,6 +16,7 @@ angular.module('financieraClienteApp')
     self.OrdenPago = {};
     self.OrdenPago.Vigencia = 2017;
     self.DataNomininaSelet = {};
+    self.DataNominaTitanSelect = {};
     // unidad ejecutora
     financieraRequest.get('unidad_ejecutora',
       $.param({
@@ -41,16 +42,60 @@ angular.module('financieraClienteApp')
     ).then(function(response) {
       self.subTipoOrdenPago = response.data;
     });
+    // Funcion encargada de validar la obligatoriedad de los campos
+    self.camposObligatorios = function() {
+      self.MensajesAlerta = '';
+      if (Object.keys(self.DataNominaTitanSelect).length == 0) {
+        self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_LIQUIDACION') + "</li>";
+      }
+      if (self.OrdenPago.SubTipoOrdenPago == undefined) {
+        self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_TIPO_OP') + "</li>";
+      }
+      if (self.OrdenPago.FormaPago == undefined) {
+        self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_FORMA_PAGO_OP') + "</li>";
+      }
+      // if (Object.keys(self.Conceptos).length == 0) {
+      //   self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_CONCEPTO') + "</li>";
+      // }
+      // if (self.TotalAfectacion != self.OrdenPago.ValorBase) {
+      //   self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_DEBE_TOTAL_AFECTACION') + "</li>" + self.TotalAfectacion + " != " + self.OrdenPago.ValorBase;
+      // }
+      // if (Object.keys(self.afectacionEnRubros).length != 0 && Object.keys(self.saldoDeRubros).length != 0) {
+      //   angular.forEach(self.afectacionEnRubros, function(afectacionValue, afectacionKey) {
+      //     angular.forEach(self.saldoDeRubros, function(saldoValue, saldoKey) {
+      //       if (saldoKey == afectacionKey && afectacionValue > saldoValue) {
+      //         self.MensajesAlerta = self.MensajesAlerta + "<li>" + $translate.instant('MSN_TOTAL_AECTACION') + " " + afectacionKey + " " + $translate.instant('MSN_SUPERA_SALDO') + "</li>";
+      //       }
+      //     })
+      //   })
+      // }
+      // Operar
+      if (self.MensajesAlerta == undefined || self.MensajesAlerta.length == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
     //
     self.registrarOpMasivo = function() {
+      if (self.camposObligatorios()) {
+        console.log("operar");
+      } else {
+        swal({
+          title: 'Error!',
+          html: '<ol align="left">' + self.MensajesAlerta + '</ol>',
+          type: 'error'
+        })
+      }
       console.log("Data Sen");
       self.dataSen = {};
       self.dataSen.OrdenPago = self.OrdenPago;
       self.dataSen.InfoGeneralOp = self.DataHomologacion;
       console.log(self.dataSen);
-      financieraMidRequest.post('orden_pago_nomina/PreviewCargueMasivoOp', self.dataSen)
-        .then(function(data) {
-          console.log(data);
-        })
+      // financieraMidRequest.post('orden_pago_nomina/PreviewCargueMasivoOp', self.dataSen)
+      //   .then(function(data) {
+      //     console.log(data);
+      //   })
     }
   });
