1ff545c277d7a963a10d5ff31e86d5341850d5e2
Fabian León <fadarsaleeing@gmail.com>
2017-11-29 16:49:28 -0500

    #18063# Se construye crud para formulario de Legalización por práctica académica

--- app/scripts/directives/loading.js
+++ app/scripts/directives/loading.js
@@ -7,20 +7,27 @@
  * # loading
  */
 angular.module('financieraClienteApp')
-  .directive('loading', function () {
-    return {
-      restrict: 'E',
-      scope:{
-          load:'=',
-          tam:'=?'
-        },
-      template: '<div class="loading" ng-show="load">' +
-                   '<i class="fa fa-clock-o fa-spin fa-{{tam}}x faa-burst animated " aria-hidden="true" ></i>' +
-                   '</div>',
-      controller:function($scope){
-        if ($scope.tam===undefined) {
-          $scope.tam=5;
-        }
-      }
-    };
-  });
+    .directive('loading', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                load: '=',
+                tam: '=?',
+                clase: "="
+            },
+            template: '<div class="loading" ng-show="load">' +
+                '<i class="{{clase.clase}} fa-spin fa-{{tam}}x {{clase.animacion}}" aria-hidden="true" ></i>' +
+                '</div>',
+            controller: function($scope) {
+                if ($scope.tam === undefined) {
+                    $scope.tam = 5;
+                }
+                if ($scope.clase === undefined) {
+                    $scope.clase = {
+                        clase: "fa fa-clock-o",
+                        animacion: "faa-burst animated"
+                    };
+                }
+            }
+        };
+    });
