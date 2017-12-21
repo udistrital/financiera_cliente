b820a85c6b2136de9f489943838df99363eff03b
jjvargass <jotavargas89@gmail.com>
2017-12-21 13:03:41 -0500

    #18541# control de campos para homologacion

--- app/scripts/controllers/homologacion_concepto/homologacion_concepto_crear.js
+++ app/scripts/controllers/homologacion_concepto/homologacion_concepto_crear.js
@@ -12,6 +12,7 @@ angular.module('financieraClienteApp')
 
     var self = this;
     self.HomologacionConcepto = {};
+    self.camposFacultades = false;
 
     titanRequest.get('nomina',
       $.param({
@@ -19,10 +20,51 @@ angular.module('financieraClienteApp')
         limit: -1
       })
     ).then(function(response) {
-      self.NominaTitanData = response.data
+      self.nominaTitanData = response.data
     })
     //
+    financieraRequest.get('concepto',
+      $.param({
+        query: "Id:73",
+        limit: -1
+      })
+    ).then(function(response) {
+      self.facultades = response.data;
+    })
+    //
+    self.getIdNomina = function(nominaSelect) {
+      if (nominaSelect != undefined) {
+        self.HomologacionConcepto.NominaTitan = nominaSelect.Id;
+        if (nominaSelect.TipoNomina.Nombre == 'HCS') {
+          self.camposFacultades = true;
+        } else {
+          self.camposFacultades = false;
+          delete self.HomologacionConcepto['Facultad']
+          delete self.HomologacionConcepto['ProyectoCurricular']
+        }
+      } else {
+        delete self.HomologacionConcepto['NominaTitan']
+      }
+    }
+    //
+    self.getIdFacultad = function(facultadSelect) {
+      if (facultadSelect != undefined) {
+        self.HomologacionConcepto.Facultad = facultadSelect.Id;
+      } else {
+        delete self.HomologacionConcepto['Facultad']
+      }
+    }
+    //
+    self.getIdProyectoCurricular = function(proyectoCurricularSelect) {
+      if (proyectoCurricularSelect != undefined) {
+        self.HomologacionConcepto.ProyectoCurricular = proyectoCurricularSelect.Id;
+      } else {
+        delete self.HomologacionConcepto['ProyectoCurricular']
+      }
+    }
+    //
     self.gridConcepto = {
+      enableRowSelection: true,
       enableRowHeaderSelection: true,
       paginationPageSizes: [8, 50, 100],
       enableFiltering: true,
@@ -47,22 +89,64 @@ angular.module('financieraClienteApp')
     };
     self.gridConcepto.multiSelect = false;
     self.gridConcepto.onRegisterApi = function(gridApi) {
-      self.gridApi = gridApi;
-      gridApi.selection.on.rowSelectionChanged($scope, function(row) {
-        console.log(row.entity);
-        self.ConceptoSelect = row.entity.Id;
-        console.log(self.ConceptoSelect);
+      $scope.gridApi = gridApi;
+      gridApi.selection.on.rowSelectionChanged($scope, function() {
+        self.ConceptoSelect = $scope.gridApi.selection.getSelectedRows()[0];
+        self.HomologacionConcepto.ConceptoKronos = self.ConceptoSelect.Id;
       });
     };
-
     financieraRequest.get("concepto",
       $.param({
         query: "Clasificador:false",
         sortby: "Codigo",
         limit: -1,
-        order: "asc",      })
+        order: "asc",
+      })
     ).then(function(response) {
       self.gridConcepto.data = response.data;
+    });
+    //
+    self.gridConceptoTitan = {
+      enableRowSelection: true,
+      enableRowHeaderSelection: true,
+      paginationPageSizes: [8, 50, 100],
+      enableFiltering: true,
+      minRowsToShow: 8,
+      useExternalPagination: false,
+      columnDefs: [{
+          field: 'Id',
+          visible: false
+        },
+        {
+          field: 'Id',
+          displayName: $translate.instant('CODIGO') + " " + $translate.instant('CONCEPTO') + " Titan",
+          width: '20%',
+          cellClass: 'input_center',
+        },
+        {
+          field: 'AliasConcepto',
+          displayName: $translate.instant('NOMBRE') + " " + $translate.instant('CONCEPTO') + " Titan",
+          width: '80%',
+        }
+      ],
+    };
+    self.gridConceptoTitan.multiSelect = false;
+    self.gridConceptoTitan.onRegisterApi = function(gridApi) {
+      $scope.gridApi2 = gridApi;
+      gridApi.selection.on.rowSelectionChanged($scope, function() {
+        self.ConceptoTitanSelect = $scope.gridApi2.selection.getSelectedRows()[0];
+        self.HomologacionConcepto.ConceptoTitan = self.ConceptoTitanSelect.Id;
+      });
+    };
+    titanRequest.get("concepto_nomina",
+      $.param({
+        query: "NaturalezaConcepto.Nombre:devengo",
+        sortby: "Id",
+        limit: -1,
+        order: "asc",
+      })
+    ).then(function(response) {
+      self.gridConceptoTitan.data = response.data;
     });
 
 
