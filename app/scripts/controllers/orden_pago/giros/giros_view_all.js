2e60db6d4ba9de9679a792476f8e1a3ae315cb48
jjvargass <jotavargas89@gmail.com>
2018-01-16 12:59:07 -0500

    git stat

--- app/scripts/controllers/orden_pago/giros/giros_view_all.js
+++ app/scripts/controllers/orden_pago/giros/giros_view_all.js
@@ -10,6 +10,5 @@
 angular.module('financieraClienteApp')
   .controller('OpGirosViewAllCtrl', function ($scope, financieraRequest, $window, $translate) {
     var self = this;
-    self.estado = "EOP_07";
-    self.op_seleccionadas = [];
+
   });
