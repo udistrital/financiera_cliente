885e64ae9d677d9aa69bd47001df0e076f0835fb
sistemas <sistemas@localhost.localdomain>
2018-02-13 17:19:04 -0500

    Cambios en el cliente para ingresos

--- app/scripts/controllers/pac/cierre_periodo.js
+++ app/scripts/controllers/pac/cierre_periodo.js
@@ -8,67 +8,22 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('PacCierrePeriodoCtrl', function ($scope, $translate) {
+  .controller('PacCierrePeriodoCtrl', function ($scope, $translate,financieraMidRequest) {
   	var ctrl = this;
+    
+    ctrl.generarCierre = function(){
 
-  	
-  	var monthFormat = buildLocaleProvider("MMM-YYYY");
-  	var ymdFormat = buildLocaleProvider("YYYY-MM-DD");
-
-
-  function buildLocaleProvider(formatString) {
-        return {
-            formatDate: function (date) {
-                if (date) return moment(date).format(formatString);
-                else return null;
-            },
-            parseDate: function (dateString) {
-                if (dateString) {
-                    var m = moment(dateString, formatString, true);
-                    return m.isValid() ? m.toDate() : new Date(NaN);
-                }
-                else return null;
-            }
-        };
+      var consulta = {
+        inicio: ctrl.fechaInicio,
+        fin: new Date(ctrl.fechaInicio.getFullYear(),ctrl.fechaInicio.getMonth() + 1,0),
+        codigo:'2'
+      };
+      console.log("fecha fin "+ consulta.fin);
+      financieraMidRequest.post('rubro/GenerarCierre', consulta).then(function(response){
+    });  
     }
 
 
-  	$scope.dateFields = [
-                {
-                    type: 'date',
-                    required: false,
-                    binding: 'applicant.pickADate',
-                    label: 'Standard Date Field - picking day, month and year',
-                    startView: 'day',
-                    mode: 'day',
-                    locale: ymdFormat
-                },
-                {
-                    type: 'date',
-                    required: true,
-                    binding: 'applicant.dateOfBirth',
-                    label: 'Date of Birth (date picker that starts with month and year, but still needs day)',
-                    startView: 'month',
-                    mode: 'day',
-                    locale: ymdFormat
-                },
-                {
-                    type: 'date',
-                    required: false,
-                    binding: 'applicant.expectedGraduation',
-                    startView: 'month',
-                    label: 'Credit Card Expiry - Year/Month picker',
-                    mode: 'month',
-                    locale: monthFormat
-                }
-    ];
-  	
-
-
-  	ctrl.incrementaMes = function(){
- 		console.log(1);
-
- 	}
 
     });
 
