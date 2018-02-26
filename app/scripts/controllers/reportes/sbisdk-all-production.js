edd5eedea2283a0351fb444744075d06466a0944
miguelramirez93 <darckzios@gmail.com>
2018-02-26 14:29:34 -0500

    se corrige variable problematica en build

--- app/scripts/controllers/reportes/sbisdk-all-production.js
+++ app/scripts/controllers/reportes/sbisdk-all-production.js
@@ -4,7 +4,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
 
-Sbi = this.Sbi || {};
+var Sbi =  {};
 Sbi.sdk = {version: '2.2'};
 
 /**
