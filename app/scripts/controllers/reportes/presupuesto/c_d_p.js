9bba9df4fbaa8dcb1a61e21590f181926947382a
jcamilosarmientor <jcamilosarmientor@gmail.com>
2018-02-26 13:56:18 -0500

    feact(certificado-cdp): se consume la inforamción del cuerp del reporte

--- app/scripts/controllers/reportes/presupuesto/c_d_p.js
+++ app/scripts/controllers/reportes/presupuesto/c_d_p.js
@@ -8,15 +8,17 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('ReportesPresupuestoCDPCtrl', function (financieraRequest, oikosRequest, $scope) {
+  .controller('ReportesPresupuestoCDPCtrl', function (financieraRequest, oikosRequest, administrativaPruebasRequest) {
     var ctrl = this;
 
-    // Vigencias de apropiaciones
-    financieraRequest.get('apropiacion/VigenciaApropiaciones', $.param({
-      limit: 0
-    })).then(function(response) {
-      ctrl.vigencias = response.data;
-    });
+    ctrl.vigencias = [2017,2018]
+    // // Vigencias de apropiaciones
+    // financieraRequest.get('apropiacion/VigenciaApropiaciones', $.param({
+    //   limit: 0
+    // })).then(function(response) {
+    //   console.log(response.data);
+    //   ctrl.vigencias = response.data;
+    // });
 
     // Unidades ejecutoras
     financieraRequest.get('unidad_ejecutora', $.param({
@@ -38,5 +40,84 @@ angular.module('financieraClienteApp')
       order: "asc"
     })).then(function(response) {
       ctrl.dependencias = response.data;
-    })
+    });
+
+    function entidad(idEntidad) {
+      financieraRequest.get('entidad', $.param({
+        limit: 0,
+        query: 'Id:'+idEntidad
+      })).then(function(response) {
+        if (response.data !== null) {
+          console.log('entidad: ', response.data);
+        }
+      });
+    }
+
+    function rubro(idRubro) {
+      financieraRequest.get('rubro', $.param({
+        limit: 0,
+        query: 'Id:'+idRubro
+      })).then(function(response) {
+        console.log('rubro: ',response.data);
+      });
+    }
+
+    // Apropiacion
+    function apropiacion(idApropiacion) {
+      financieraRequest.get('apropiacion', $.param({
+        limit: 0,
+        query: 'Id:'+idApropiacion
+      })).then(function(response) {
+        if (response.data !== null) {
+          console.log('apropiacion: ',response.data);
+          rubro(response.data[0].Rubro.Id)
+        }
+      });
+    }
+
+    // Fuente de finaciamiento apropiacion
+    function fuentFinanApropiacion(idFuenteFinanciamiento) {
+      financieraRequest.get('fuente_financiamiento_apropiacion', $.param({
+        limit:1,
+        query: 'FuenteFinanciamiento.Id:'+idFuenteFinanciamiento+',Dependencia:'+ctrl.dependencia.Id
+      })).then(function(response) {
+        if (response.data !== null) {
+          console.log("fuente_finacim eianto_apropiacion: ",response.data);
+          apropiacion(response.data[0].Apropiacion.Id);
+        }
+      })
+    }
+
+    // Fuente de financiamiento
+    function fuentFinan(idFuente) {
+      financieraRequest.get('fuente_financiamiento', $.param({
+        limit: 1,
+        query: 'Id:'+idFuente
+      })).then(function(response) {
+        if (response.data !== null) {
+          console.log("fuente financiamiento: ", response.data);
+          fuentFinanApropiacion(response.data[0].Id);
+        }
+      });
+    }
+
+
+    ctrl.generarReporte = function() {
+      console.log(ctrl.unidadEjecutora);
+      entidad(ctrl.unidadEjecutora.Entidad.Id)
+      administrativaPruebasRequest.get('fuente_financiacion_rubro_necesidad', $.param({
+        limit: 1,
+        query: 'Necesidad.Numero:'+ctrl.necesidad+',Necesidad.Vigencia:'+ctrl.vigencia
+      })).then(function(response) {
+        if (response.data === null) {
+          swal(
+            'No se encontraron datos que coincidan con la necesidad y la vigencia'
+          )
+        } else {
+          fuentFinan(response.data[0].FuenteFinanciamiento);
+        }
+        //ctrl.necesidad = response
+      });
+    }
+
   });
