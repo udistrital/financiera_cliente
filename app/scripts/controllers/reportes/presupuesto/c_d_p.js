ce50e8e5eaafbac0d10dd14c7b801aaebc73c4e1
jcamilosarmientor <jcamilosarmientor@gmail.com>
2018-02-28 19:06:50 -0500

    feature(cdp_reporte): certificado CDP sin mostrar jefe de dependencia

--- app/scripts/controllers/reportes/presupuesto/c_d_p.js
+++ app/scripts/controllers/reportes/presupuesto/c_d_p.js
@@ -10,7 +10,56 @@
 angular.module('financieraClienteApp')
   .controller('ReportesPresupuestoCDPCtrl', function (financieraRequest, oikosRequest, administrativaPruebasRequest, $q) {
     var ctrl = this;
-    var docDefinition = {};
+    var reporte = { content: [], styles: {}};
+
+    // Estilos del reporte
+    var estilos = {
+      header: {
+        fontSize: 16,
+        bold: true,
+        alignment: 'center',
+        margin: [0,0,0,20]
+      },
+      subheader: {
+        fontSize: 13,
+        bold: true,
+        alignment: 'center',
+        margin: [10,0,0,0]
+      },
+      subheader_part: {
+        fontSize: 13,
+        bold: true,
+        alignment: 'center',
+        margin: [0,0,0,10]
+      },
+      paragraph: {
+        fontSize: 11,
+        alignment: 'center',
+        margin: [0,0,0, 20]
+      },
+      rubro_table: {
+        marign: [10,20,10,20],
+        alignment: 'center',
+        border: undefined
+      },
+      table_header: {
+        fontSize: 12,
+        alignment: 'center',
+        bold: true,
+        margin: [2,2,2,2]
+      },
+      table_content: {
+        fontSize: 12,
+        margin: [0,5,0,0],
+        alignment: 'center',
+        border: undefined
+      },
+      objeto: {
+        fontSize: 12,
+        margin: [0,5,5,0],
+        alignment: 'center'
+      }
+    }
     ctrl.vigencias = [2017,2018]
     // // Vigencias de apropiaciones
     // financieraRequest.get('apropiacion/VigenciaApropiaciones', $.param({
@@ -113,6 +162,7 @@ angular.module('financieraClienteApp')
         query: 'TipoFuenteFinanciamiento.Id:'+ctrl.tipoFuenteFinanciamiento.Id
       })).then(function(response) {
         defered.resolve(response.data);
+        return response.data;
       }, function(err) {
         defered.reject(err);
       });
@@ -121,6 +171,8 @@ angular.module('financieraClienteApp')
     }
 
     ctrl.generarReporte = function() {
+      reporte.styles = estilos;
+      var entidad;
       administrativaPruebasRequest.get('fuente_financiacion_rubro_necesidad', $.param({
         limit: 1,
         query: 'Necesidad.Numero:'+ctrl.necesidad+',Necesidad.Vigencia:'+ctrl.vigencia
@@ -130,11 +182,10 @@ angular.module('financieraClienteApp')
             'No se encontraron datos que coincidan con la necesidad y la vigencia'
           )
         } else {
-          docDefinition = {text: 'UNIVERSIDAD DISTRITAL FRANCISCO JOSÉ DE CALDAS'}
-
+          ctrl.fuenteFinanciacionNecesidad = response.data[0];
           asynFuentFinan(response.data[0].FuenteFinanciamiento)
             .then(function(data) {
-              console.log(data);
+              ctrl.fuentesFinanciamiento = data;
 
                 asynFuentFinanApropiacion(data.Id)
                   .then(function(data) {
@@ -142,18 +193,53 @@ angular.module('financieraClienteApp')
 
                     asynApropiacion(data[0].Apropiacion.Id)
                       .then(function(data) {
-                        console.log(data);
+                        ctrl.apropiacion = data;
 
                         asynRubro(data.Rubro.Id)
                           .then(function(data) {
-                            console.log(data);
+                            ctrl.rubro = data;
+
+                            asynEntidad()
+                              .then(function(data) {
+                                reporte.content.push(
+                                  { text: data.Nombre, style: 'header' },
+                                  { text: data.CodigoEntidad+' - '+data.Nombre, style: 'subheader' },
+                                  { text: ctrl.unidadEjecutora.Id+' - '+ctrl.unidadEjecutora.Nombre, style: 'subheader_part' },
+                                  { text: 'CERTIFICADO DE DISPONIBILIDAD PRESUPUESTAL', style: 'subheader' },
+                                  { text: 'No.   1', style: 'subheader_part' },
+                                  { text: 'EL SUSCRITO RESPONSABLE DEL PRESUPUESTO', style: 'subheader' },
+                                  { text: 'CERTIFICA', style: 'subheader_part' },
+                                  { text: 'Que en el Presupuesto de Gastos e Inversiones de la vigencia '+ctrl.vigencia+' existe apropiación disponible para atender a la presente solicitud así: ', style: 'paragraph' },
+                                  {
+                                    style: 'rubro_table',
+                                    table:
+                                    {
+                                      headerRows: 1,
+                                      widths: ['40%', '40%', '20%'],
+                                      body:
+                                        [
+                                          [{ text: 'CODIGO PRESUPUESTAL ', style: 'table_header'}, { text: 'CONCEPTO', style: 'table_header'}, { text: 'VALOR', style: 'table_header'}],
+                                          [{ text: ctrl.rubro.Codigo, style: 'table_content' }, { text: ctrl.rubro.Nombre, style: 'table_content'}, { text: ctrl.apropiacion.Valor, style: 'table_content'}]
+                                        ]
+                                      }
+                                  },
+                                  { text: 'OBJETO:', bold: true, margin: [0,20,0,10] },
+                                  { text:  ctrl.fuenteFinanciacionNecesidad.Necesidad.Objeto, style: 'objeto'});
+
+                                pdfMake.createPdf(reporte).download('cdp.pdf');
+                            }).catch(function(err) {
+                                console.error(err);
+                            });
+
                           }).catch(function(err) {
                             console.log(error);
                           });
 
+
                       }).catch(function(err) {
                         console.error(err);
                       });
+
 
                   }).catch(function(err) {
                     console.error(err);
@@ -163,17 +249,10 @@ angular.module('financieraClienteApp')
               console.error(err);
             });
 
-            asynEntidad()
-              .then(function(data) {
-                console.log(data);
-              }).catch(function(err) {
-                console.error(err);
-              });
-
         }
-    }, function(err) { //if something happends
-    });
+      }, function(err) { //if something happends
+      });
 
+      reporte.content = [];
     }
-
   });
