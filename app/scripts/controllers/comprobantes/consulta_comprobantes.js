207c923acd8b945dbdc9b6fd43fcacb47a928bc1
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2018-03-05 16:38:15 -0500

    Se agrega vista y controlador de consulta de comprobantes

---
+++ app/scripts/controllers/comprobantes/consulta_comprobantes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name financieraClienteApp.controller:PacCierrePeriodoCtrl
+ * @description
+ * # PacCierrePeriodoCtrl
+ * Controller of the financieraClienteApp
+ */
+angular.module('financieraClienteApp')
+  .controller('ConsultaComprobantesCtrl', function ($scope, $translate,financieraMidRequest,financieraRequest) {
+  	var ctrl = this;
+
+
+    });
