5bc8a2018d0c4ca47f9aa0137a277dc60aa8f222
sistemas <sistemas@localhost.localdomain>
2018-05-15 17:12:14 -0500

    Ajustes realizados en PAC para totalizar por fuente

--- app/scripts/controllers/pac/reporte_pac.js
+++ app/scripts/controllers/pac/reporte_pac.js
@@ -55,69 +55,107 @@ angular.module('financieraClienteApp')
       }
     };
 
+
+    $scope.gridOptions_totales = {
+      enableHorizontalScrollbar: 1,
+      enableVerticalScrollbar: 1,
+      paginationPageSizes: [5, 10, 15],
+      paginationPageSize: 5,
+      enableFiltering: true,
+      enableGridMenu: true,
+      exporterCsvFilename: 'TotalesRubroPac.csv',
+      exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
+      exporterFieldCallback: function (grid, row, col, value) {
+          return grid.getCellDisplayValue(row, col);
+      },
+      onRegisterApi: function(gridApi) {
+        $scope.gridApi = gridApi;
+      }
+    };
+
     $scope.gridOptions_egresos.data = null;
     $scope.gridOptions.data=null;
 
     self.generarReporte = function() {
-      $scope.gridOptions_egresos.columnDefs =[];
-    $scope.gridOptions_egresos={
 
-    columnDefs: [{
-        name: 'Fdescrip',
-        displayName: $translate.instant('FUENTE'),
-        headerCellClass: 'text-info',
-        width: "20%",
-        pinnedLeft: true,
-        cellClass: function(grid, row) {
-                        if (row.entity.Fdescrip.indexOf("Total") !==-1) {
-                              return 'ui-grid-desc-cellT';
-                        }
+      $scope.gridOptions_egresos.columnDefs =[];
+      $scope.gridOptions_egresos={
+
+      columnDefs: [{
+          name: 'Fdescrip',
+          displayName: $translate.instant('FUENTE'),
+          headerCellClass: 'text-info',
+          width: "20%",
+          pinnedLeft: true,
+          cellClass: function(grid, row) {
+                          if (row.entity.Fdescrip.indexOf("Total") !==-1) {
+                                return 'ui-grid-desc-cellT';
+                          }
+          },
         },
-      },
+        {
+          name: 'Descripcion',
+          displayName: $translate.instant('RUBRO'),
+          headerCellClass: 'text-info',
+          width: "20%",
+          pinnedLeft: true,
+          cellClass: function(grid, row) {
+                          if (row.entity.Fdescrip.indexOf("Total") !==-1) {
+                                return 'ui-grid-desc-cellT';
+                          }
+          },
+        },
+        {
+          name: 'Codigo',
+          displayName: $translate.instant('CODIGO'),
+          headerCellClass: 'text-info',
+          width: "20%",
+          pinnedLeft: true,
+          cellClass: function(grid, row) {
+                          if (row.entity.Fdescrip.indexOf("Total") !==-1) {
+                                return 'ui-grid-desc-cellT';
+                          }
+          },
+        },
+      ],
+    };
+
+
+    $scope.gridOptions_totales={
+
+    columnDefs: [
       {
         name: 'Descripcion',
         displayName: $translate.instant('RUBRO'),
         headerCellClass: 'text-info',
-        width: "20%",
+        width: "60%",
         pinnedLeft: true,
         cellClass: function(grid, row) {
-                        if (row.entity.Fdescrip.indexOf("Total") !==-1) {
+                        if (row.entity.Descripcion.indexOf("Total") !==-1) {
                               return 'ui-grid-desc-cellT';
                         }
         },
-      },
-      {
-        name: 'Codigo',
-        displayName: $translate.instant('CODIGO'),
-        headerCellClass: 'text-info',
-        width: "20%",
-        pinnedLeft: true,
-        cellClass: function(grid, row) {
-                        if (row.entity.Fdescrip.indexOf("Total") !==-1) {
-                              return 'ui-grid-desc-cellT';
-                        }
-        },
-      },
+      }
 
-      /*{
-        name: 'Opciones',
-        cellTemplate: ' <a type="button" class="fa fa-eye" ng-click="grid.appScope.reportePac.verResumen(row)" ></a>',
-        headerCellClass: 'text-info',
-        width: "5%"
-      },*/
+
     ],
-  };
+    };
+
       var consulta = {
         inicio: self.fechaInicio,
         fin: self.fechaFin,
         periodosproy: 3
       };
       financieraMidRequest.post('rubro/GenerarPac', consulta).then(function(response) {
+        console.log(response.data);
         $scope.union = [];
+        $scope.unionTotales = [];
         $scope.ingresos = response.data.Ingresos;
         $scope.egresos =  response.data.Egresos;
-        $scope.union.push.apply($scope.union,$scope.ingresos)
-        $scope.union.push.apply($scope.union,$scope.egresos)
+        $scope.union.push.apply($scope.union,$scope.ingresos);
+        $scope.union.push.apply($scope.union,$scope.egresos);
+        $scope.unionTotales.push.apply($scope.unionTotales,response.data.TotalesIngresos);
+        $scope.unionTotales.push.apply($scope.unionTotales,response.data.TotalesEgresos);
 
         if ($scope.union != null && $scope.union != undefined){
           for (var i = 0; i < $scope.union[0].Reporte.length; i++) {
@@ -187,10 +225,77 @@ angular.module('financieraClienteApp')
                             }
             },
           });
+          $scope.gridOptions_totales.columnDefs.push({
+            name: '' + response.data.Egresos[0].Reporte[i].Mes + ' EJC',
+            field: 'Reporte[' + i + '].Valores.Valor',
+            //cellTemplate: ' <div>{{row.entity.reporte[' + i + '].valores.valor | currency}}</div>',
+            headerCellClass: 'text-info',
+            width: "8%",
+            enablePinning: false,
+            cellFilter: 'currency',
+            cellClass: function(grid, row) {
+                            if (row.entity.Descripcion.indexOf("Total") ==-1) {
+                                  return 'right-letters';
+                            }else{
+                                  return 'ui-grid-number-cellT';
+                            }
+            },
+            exporterPdfAlign: 'right'
+          });
+          $scope.gridOptions_totales.columnDefs.push({
+            name: '' + response.data.Egresos[0].Reporte[i].Mes + ' PROY',
+            field: 'Reporte[' + i + '].Valores.Proyeccion',
+            headerCellClass: 'text-info',
+            width: "8%",
+            //cellTemplate: ' <div>{{row.entity.reporte[' + i + '].valores.proyeccion}}</div>',
+            enablePinning: false,
+            cellFilter: 'currency',
+            cellClass: function(grid, row) {
+                            if (row.entity.Descripcion.indexOf("Total") ==-1) {
+                                  return 'right-letters';
+                            }else{
+                                  return 'ui-grid-number-cellT';
+                            }
+            },
+            exporterPdfAlign: 'right'
+          });
+          $scope.gridOptions_totales.columnDefs.push({
+            name: '' + response.data.Egresos[0].Reporte[i].Mes + ' VAR',
+            field: 'Reporte[' + i + '].Valores.Variacion',
+            headerCellClass: 'text-info',
+            width: "8%",
+            //cellTemplate: ' <div class="{{grid.appScope.reportePac.changeCellClass(row.entity.reporte[' + i + '].valores.variacion)}}">{{row.entity.reporte[' + i + '].valores.variacion}}</span>',
+            enablePinning: false,
+            cellFilter: 'currency',
+            cellClass: function(grid, row) {
+                            if (row.entity.Descripcion.indexOf("Total") ==-1) {
+                                  return 'right-letters';
+                            }else{
+                                  return 'ui-grid-number-cellT';
+                            }
+            },
+            exporterPdfAlign: 'right'
+          });
+          $scope.gridOptions_totales.columnDefs.push({
+            name: '' + response.data.Egresos[0].Reporte[i].Mes + ' %',
+            field: 'Reporte[' + i + '].Valores.Pvariacion',
+            headerCellClass: 'text-info',
+            width: "8%",
+            cellTemplate: ' <div class="{{grid.appScope.reportePac.changeCellClass(row.entity.Reporte[' + i + '].Valores.Pvariacion*100)}}">{{row.entity.Reporte[' + i + '].Valores.Pvariacion * 100}} %</div>',
+            enablePinning: false,
+            cellClass: function(grid, row) {
+                            if (row.entity.Descripcion.indexOf("Total") ==-1) {
+                                  return 'right-letters';
+                            }else{
+                                  return 'ui-grid-number-cellT';
+                            }
+            },
+          });
         }
         }
 
         $scope.gridOptions_egresos.data = $scope.union;
+        $scope.gridOptions_totales.data = $scope.unionTotales;
 
       });
     }
