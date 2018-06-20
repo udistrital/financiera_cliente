331500a837e3fc5b594d575f192c482fb61b8f44
Yeison <sistemas@localhost.localdomain>
2018-06-20 17:25:48 -0500

    modificaciones homologación rubros

--- app/scripts/directives/homologacion_rubro/rubro_entidad.js
+++ app/scripts/directives/homologacion_rubro/rubro_entidad.js
@@ -18,14 +18,17 @@ angular.module('financieraClienteApp')
       controller:function($scope,financieraMidRequest,$translate){
         var ctrl = this;
         ctrl.getList = function(){
-          financieraMidRequest.get('organizaciones_core_new', $.param({
+          financieraMidRequest.get('organizaciones_core_new/getOrganizacion', $.param({
               limit: -1,
               query: "CodigoAbreviacion:TE_2"
           })).then(function(response) {
-
+              ctrl.entidades=response.data;
           });
         }
         ctrl.getList();
+        ctrl.getRubros = function(){
+
+        }
     },
       controllerAs:'d_homologacionRubroRubroEntidad'
     };
