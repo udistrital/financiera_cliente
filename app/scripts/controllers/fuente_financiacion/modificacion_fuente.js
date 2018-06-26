94c270420e12405ffb681b2ec775429e8d8a2e97
mariaalejandra9404 <mariaalejandraortiz0401@gmail.com>
2018-06-25 19:55:46 -0500

    se corrige grid de fuentes que no cargaba correctamente

--- app/scripts/controllers/fuente_financiacion/modificacion_fuente.js
+++ app/scripts/controllers/fuente_financiacion/modificacion_fuente.js
@@ -29,26 +29,7 @@ angular.module('financieraClienteApp')
     });
 
     self.datos_vigencia = function() {
-      financieraRequest.get("movimiento_fuente_financiamiento_apropiacion", 'limit=-1&query=Fecha__startswith:' + parseInt(self.year)).then(function(response) {
-        self.movimiento_fuente_financiamiento_apropiacion = response.data;
-        self.fuente_financiamiento = [];
-        if (self.movimiento_fuente_financiamiento_apropiacion) {
-          for (i = 0; i < self.movimiento_fuente_financiamiento_apropiacion.length; i++) {
-            self.repetido = false;
 
-            for (j = 0; j < self.fuente_financiamiento.length; j++) {
-              if (self.movimiento_fuente_financiamiento_apropiacion[i].FuenteFinanciamientoApropiacion.FuenteFinanciamiento.Id == self.fuente_financiamiento[j].Id) {
-                self.repetido = true;
-              }
-            }
-            if (!self.repetido) {
-              self.fuente_financiamiento.push(self.movimiento_fuente_financiamiento_apropiacion[i].FuenteFinanciamientoApropiacion.FuenteFinanciamiento);
-            }
-          }
-        }
-        self.gridOptionsfuente.data = self.fuente_financiamiento;
-
-      });
 
       financieraRequest.get("apropiacion", 'limit=-1&query=Vigencia:' + parseInt(self.year) + ',rubro.codigo__startswith:3-3-001-15-01-08-0119-&sortby=rubro&order=asc').then(function(response) {
         self.apropiacion1 = response.data;
@@ -192,6 +173,28 @@ angular.module('financieraClienteApp')
     };
 
     self.mostrar_fuentes = function() {
+
+      financieraRequest.get("movimiento_fuente_financiamiento_apropiacion", 'limit=-1&query=Fecha__startswith:' + parseInt(self.year)).then(function(response) {
+        self.movimiento_fuente_financiamiento_apropiacion = response.data;
+        self.fuente_financiamiento = [];
+        if (self.movimiento_fuente_financiamiento_apropiacion) {
+          for (i = 0; i < self.movimiento_fuente_financiamiento_apropiacion.length; i++) {
+            self.repetido = false;
+
+            for (j = 0; j < self.fuente_financiamiento.length; j++) {
+              if (self.movimiento_fuente_financiamiento_apropiacion[i].FuenteFinanciamientoApropiacion.FuenteFinanciamiento.Id == self.fuente_financiamiento[j].Id) {
+                self.repetido = true;
+              }
+            }
+            if (!self.repetido) {
+              self.fuente_financiamiento.push(self.movimiento_fuente_financiamiento_apropiacion[i].FuenteFinanciamientoApropiacion.FuenteFinanciamiento);
+            }
+          }
+        }
+        self.gridOptionsfuente.data = self.fuente_financiamiento;
+
+      });
+      
       self.fuentes_seleccionadas = [];
       for (i = 0; i < self.fuente_financiamiento_apropiacion.length; i++) {
         self.codigo_fuente = self.fuente_financiamiento_apropiacion[i].FuenteFinanciamiento;
