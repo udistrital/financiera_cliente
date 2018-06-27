01b721119e8d1c65b30f88c1802264f18eeabb16
Yeison <sistemas@localhost.localdomain>
2018-06-27 16:01:02 -0500

    generar arbol rubros homologados

--- app/scripts/directives/homologacion_rubro/rubro_entidad.js
+++ app/scripts/directives/homologacion_rubro/rubro_entidad.js
@@ -10,13 +10,36 @@ angular.module('financieraClienteApp')
   .directive('homologacionRubroEntidad', function () {
     return {
       restrict: 'E',
-      /*scope:{
-          var:'='
+      scope:{
+        seleccion: '=?',
+        filtro: '=?',
+        rubrosel: '=?',
+        recargar: '=?',
+        rubroid: '=?',
+        arbol: '=?',
+        noresumen: '@?',
+        ramasel: '=?',
+        botones: '=?',
         },
-      */
+
       templateUrl: 'views/directives/homologacion_rubro/rubro_entidad.html',
       controller:function($scope,financieraMidRequest,$translate){
         var ctrl = this;
+        ctrl.expandedNodes = [];
+        ctrl.treeOptions = {
+          nodeChildren: "Hijos",
+          dirSelectable: $scope.ramasel,
+          injectClasses: {
+            ul: "a1",
+            li: "a2",
+            liSelected: "a7",
+            iExpanded: "a3",
+            iCollapsed: "a4",
+            iLeaf: "a5",
+            label: "a6",
+            labelSelected: "a8"
+          }
+        };
         ctrl.getList = function(){
           financieraMidRequest.get('organizaciones_core_new/getOrganizacion', $.param({
               limit: -1,
@@ -28,6 +51,16 @@ angular.module('financieraClienteApp')
         ctrl.getList();
         ctrl.getRubros = function(){
 
+        }
+        ctrl.ArbolRubrosEntidad = function(){
+          financieraMidRequest.get('rubro_homologado/GetArbolRubrosHomologado',$.param({
+            idEntidad:ctrl.entidad.Id
+          })).then(function(response){
+            $scope.arbol = [];
+            if (response.data !== null) {
+              $scope.arbol = response.data;
+            }
+          });
         }
     },
       controllerAs:'d_homologacionRubroRubroEntidad'
