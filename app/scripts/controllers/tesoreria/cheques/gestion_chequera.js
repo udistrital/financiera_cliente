200d5fc58192e99d41b8648a2fc6c3fb5d909780
Yeison <sistemas@localhost.localdomain>
2018-08-14 17:05:23 -0500

    gestion chequeras, agregar valores listas

--- app/scripts/controllers/tesoreria/cheques/gestion_chequera.js
+++ app/scripts/controllers/tesoreria/cheques/gestion_chequera.js
@@ -8,11 +8,12 @@
  * Controller of the financieraClienteApp
  */
 angular.module('financieraClienteApp')
-  .controller('TesoreriaChequesGestionChequeraCtrl', function ($scope,$translate) {
+  .controller('TesoreriaChequesGestionChequeraCtrl', function ($scope,$translate,agoraRequest,financieraRequest,organizacionRequest,financieraMidRequest) {
     var ctrl = this;
     $scope.botones = [
       { clase_color: "editar", clase_css: "fa fa-product-hunt fa-lg faa-shake animated-hover", titulo: $translate.instant('ESTADO'), operacion: 'proceso', estado: true }
     ];
+    ctrl.listasCargadas = false;
     ctrl.gridChequeras = {
       enableFiltering: true,
       enableSorting: true,
@@ -69,4 +70,67 @@ angular.module('financieraClienteApp')
           }
       ]
     }
+
+    ctrl.consultarListas = function(){
+      if(ctrl.listasCargadas){
+        financieraRequest.get("orden_pago/FechaActual/2006").then(function(response) {
+          var year = parseInt(response.data);
+          ctrl.anos = [];
+          for (var i = 0; i < 5; i++) {
+            ctrl.anos.push(year - i);
+          }
+        });
+
+        financieraRequest.get('unidad_ejecutora', $.param({
+            limit: -1
+        })).then(function(response) {
+            ctrl.unidadesejecutoras = response.data;
+        });
+
+        agoraRequest.get('parametro_estandar',$.param({
+          query:"ClaseParametro:Tipo Documento",
+          limit:-1
+        })).then(function(response){
+          ctrl.tiposdoc = response.data;
+        });
+
+        organizacionRequest.get('organizacion/', $.param({
+            limit: -1,
+            query: "TipoOrganizacion.CodigoAbreviacion:EB",
+        })).then(function(response) {
+          ctrl.bancos = response.data;
+        });
+
+
+      }
+
+    };
+
+    ctrl.obtenerSucursales = function(){
+      ctrl.sucursales = [];
+      financieraMidRequest.get('gestion_sucursales/ListarSucursalesBanco/'+ctrl.banco.Id).then(function(response){
+        if (response.data != null) {
+            ctrl.sucursales = response.data;
+        }
+      });
+    }
+
+    ctrl.consultaResponsable = function(){
+      agoraRequest.get('supervisor_contrato',$.param({
+        query:"Documento:" + ctrl.fila.Devolucion.Solicitante,
+        limit:-1
+      })).then(function(response){
+          if(!angular.isUndefined(response.data) && typeof(response.data) !== "string"){
+              ctrl.nombreSolicitante = response.data[0].Nombre;
+              ctrl.cargando_sol = false;
+          }else{
+            ctrl.nombreSolicitante = $translate.instant('NO_ENCONTRADO');
+            ctrl.cargando_sol = false;
+          }
+        });
+    }
+
+
+
+
   });
