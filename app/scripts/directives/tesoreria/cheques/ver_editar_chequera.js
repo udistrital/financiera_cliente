8ab659f85856aac9bf0df5f079d7e8faab414426
Yeison <sistemas@localhost.localdomain>
2018-09-03 13:25:59 -0500

    ajustar rubros_por_rp_seleccion_multiple.js para hacer build

--- app/scripts/directives/tesoreria/cheques/ver_editar_chequera.js
+++ app/scripts/directives/tesoreria/cheques/ver_editar_chequera.js
@@ -19,9 +19,6 @@ angular.module('financieraClienteApp')
       controller:function($scope,$attrs){
         var ctrl = this;
         $scope.ver = 'ver' in $attrs;
-        $scope.$watch('chequera',function(newValue){
-          console.log("chequerainformacion ",newValue);
-        },true);
       },
       controllerAs:'d_tesoreriaVerEditarChequera'
     };
