e6f98b929ec12a5bdae310a415399595ce32bef9
davidmm07 <moralesl07@hotmail.com>
2018-12-14 14:38:07 -0500

    se cambia vista rubro-concepto

--- app/scripts/directives/orden_pago/proveedor/op_proveedor_ver_por_id.js
+++ app/scripts/directives/orden_pago/proveedor/op_proveedor_ver_por_id.js
@@ -20,8 +20,11 @@ angular.module('financieraClienteApp')
       controller: function($scope) {
         var self = this;
         self.rubros = [];
+        self.conceptos = [];
         self.rps = [];
         self.tipoDocumentoOp = "1";
+        self.panel_abierto = "ninguno";
+        self.open = false;
         // paneles
         $scope.panelUnidadEjecutora = !$scope.inputpestanaabierta;
         $scope.panelProveedor = !$scope.inputpestanaabierta;
@@ -67,6 +70,45 @@ angular.module('financieraClienteApp')
               }
             });
           }
+
+          self.conceptos = [];
+          self.cargando = true;
+          self.hayData = true;
+          console.log("Hola soy tu op", $scope.opproveedorid);
+          if ($scope.opproveedorid != undefined) {
+            financieraRequest.get('concepto_orden_pago',
+            $.param({
+              query: "OrdenDePago.Id:" + $scope.opproveedorid,
+              limit: 0
+            })
+          ).then(function(response) {
+
+            if(response.data === null){
+              self.hayData = false;
+              self.cargando = false;
+              self.conceptos = [];
+
+              //quitar repetidos
+            }else{
+              self.hayData = true;
+              self.cargando = false;
+              var hash = {};
+              response.data = response.data.filter(function(current) {
+                var exists = !hash[current.RegistroPresupuestalDisponibilidadApropiacion.Id] || false;
+                hash[current.RegistroPresupuestalDisponibilidadApropiacion.Id] = true;
+                return exists;
+              });
+              //fin quitar repetidos
+              self.conceptos = response.data;
+              console.log("Hola soy tu concepto", self.conceptos);
+              // recorrer para subgrip
+
+            }
+          });
+
+
+        }
+
         })
 
         // documento
@@ -147,7 +189,29 @@ angular.module('financieraClienteApp')
           ).then(function(response) {
             self.entrada = response.data[0];
           });
-        }
+        };
+        self.abrir_panel = function(nombre){
+
+          if(self.panel_abierto === nombre && self.open === true){
+              self.open = false;
+              self.panel_abierto = "ninguno"
+          }else{
+            self.panel_abierto = nombre;
+            if(self.open === false){
+              self.open = true;
+          }else{
+              if(self.open === true){
+                self.open = false;
+
+              }
+            }
+          }
+
+
+
+        };
+
+
         //
       },
       controllerAs: 'd_opProveedorVerPorId'
